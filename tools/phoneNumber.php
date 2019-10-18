eef1e1817b432e1812a36b1a6f85dc25b59c662e
zhangchenglin <469946668@qq.com>
2019-10-18 14:42:29 +0800

    Initial commit

--- tools/phoneNumber.php
+++ tools/phoneNumber.php
@@ -15,19 +15,22 @@ require_once 'header.php';
                 <div class="form-group col-12 col-sm-6">
                     <label class="sr-only" for="phone_number_input">单位名称、简称或者号码</label>
                     <input class="form-control fa text-center" type="search" id="phone_number_input" minlength="2"
-                           maxlength="15" placeholder="单位名称、简称或者号码&nbsp;&#xf015;&nbsp;&#xf095;&nbsp;&#xf3cd;">
+                           maxlength="15" placeholder="单位名称、简称或者号码&nbsp;&nbsp;&#xf015;&nbsp;&nbsp;&#xf095;&nbsp;&nbsp;&#xf3cd;">
                 </div>
             </div>
             <div class="d-flex justify-content-center">
-                <a href="javascript:" class="btn btn-sm btn-success mx-2" id="phone_name_search">按 名称 搜索</a>
-                <a href="javascript:" class="btn btn-sm btn-danger mx-2" id="phone_number_search">按 号码 搜索</a>
+                <a href="javascript:" class="mx-2 btn btn-success" id="phone_name_search">
+                    <i class="fa fa-home">&nbsp;&nbsp;</i>按名称搜索</a>
+                <a href="javascript:" class="mx-2 btn btn-danger" id="phone_number_search">
+                    <i class="fa fa-phone">&nbsp;&nbsp;</i>
+                    <i class="fa fa-mobile-alt">&nbsp;&nbsp;</i>按号码搜索</a>
             </div>
         </div>
 
 
         <div class="container mt-4" id="phone_search_result">
             <span class="font-weight-bold text-success">查询结果</span>
-            <div class="mt-1 border border-info rounded p-5"></div>
+            <div class="mt-1 p-5 border border-info rounded"></div>
         </div>
 
 
@@ -36,7 +39,7 @@ require_once 'header.php';
             <div class="p-3 border border-info rounded">
                 <div class="my-2 form-row">
                     <div class="form-group col-12 col-sm-12 col-md-3">
-                        <label class="sr-only" for="phone_name">名称</label>
+                        <label class="sr-only" for="phone_name">名称&nbsp;<i class="fa fa-home"></i></label>
                         <input class="form-control fa text-success text-center" type="text" id="phone_name"
                                minlength="4" maxlength="9" placeholder="单位名称&nbsp;&#xf015;">
                     </div>
@@ -57,7 +60,7 @@ require_once 'header.php';
                 <div class="my-2 d-flex justify-content-center">
                     <button class="mx-2 btn btn-primary rounded-circle d-none" type="button" id="phone_number_add"
                             title="添加一行"><i class="fa fa-plus-circle"></i></button>
-                    <button class="btn btn-primary" id="phone_number_submit">提交新号码</button>
+                    <button class="btn btn-lg btn-primary" id="phone_number_submit">提交新号码</button>
                 </div>
 
             </div>
