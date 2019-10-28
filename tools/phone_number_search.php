4548bb3e584a766b79901518b56370cd6ccd9a4a
zhangchenglin <469946668@qq.com>
2019-10-29 03:00:42 +0800

    Initial commit

--- tools/phone_number_search.php
+++ tools/phone_number_search.php
@@ -17,7 +17,7 @@ if ($db->getLastErrno()) {
 };
 
 $query_key = $_POST['phone_number_search']['search_value'];
-$result_columns = ["id", "phone_name", "tel_number", "mobile_number"];
+$result_columns = ["id", "phone_name", "phone_nick_name", "tel_number", "mobile_number"];
 
 $static = "no";
 $regional = "xingmei";
@@ -33,11 +33,12 @@ switch ($_POST['phone_number_search']['search_type']) {
         $query = $db->get("phone_number", null, $result_columns);
         break;
     case "name":
-    default:
         $db->orHaving("phone_name", "%$query_key%", 'LIKE');
         $db->orHaving("phone_nick_name", "%$query_key%", 'LIKE');
 
         $query = $db->get("phone_number", null, $result_columns);
+        break;
+    default:
 }
 
 echo json_encode($query);
