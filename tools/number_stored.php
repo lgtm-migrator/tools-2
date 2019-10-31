4f3222cbedd0d66374d36000d5e39a824e48927c
zhangchenglin <469946668@qq.com>
2019-10-31 13:56:11 +0800

    Initial commit

--- tools/number_stored.php
+++ tools/number_stored.php
@@ -1,10 +1,15 @@
 <?php
 if (isset($_POST['number_stored'])) {
     require_once "mysqli.php";
+    $result = table_num_rows("jzeg_tools/phone_number");
+    echo $result;
+}
+
+function table_num_rows($database_table)
+{
+    global $db_host, $db_user, $db_pwd;
     $db = new MysqliDb($db_host, $db_user, $db_pwd, "information_schema");
-    $db->where("NAME", "jzeg_tools/phone_number");
-
+    $db->where("NAME", $database_table);
     $query = $db->get("INNODB_TABLESTATS", null, "NUM_ROWS");
-
-    echo $query[0]["NUM_ROWS"];
+    return $query[0]["NUM_ROWS"];
 }
