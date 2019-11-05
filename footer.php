c0541fe15918e3e1705932f431ed867a90e13def
zhangchenglin <469946668@qq.com>
2019-11-05 16:28:08 +0800

    Initial commit

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 ?>
-<div id="jt_footer">
+<div id="jt_footer" style="min-height: 10vh;">
     <a class="position-fixed border border-primary rounded p-1" href="javascript:" id="to_top"
        style="right: 1.2rem;bottom: 1rem;">
         <i class="fa fa-2x fa-arrow-up"></i>
@@ -12,7 +12,7 @@
     <script src="/tools/static/js/popper.min.js"></script>
     <script src="/tools/static/js/bootstrap.min.js"></script>
     <script src="/tools/static/js/bootstrap-modal-js.min.js"></script>
-    <script src="/tools/phone_number/js/phone_number.js"></script>
+    <script src="/tools/phone_number/js/phone_number.min.js"></script>
 </div>
 </body>
 </html>
