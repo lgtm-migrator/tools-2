8d23f98d3684b466d865f8c7af290e10ee4f0dcc
zhangchenglin <469946668@qq.com>
2019-11-08 23:39:04 +0800

    Initial commit

--- footer.php
+++ footer.php
@@ -5,6 +5,17 @@
        style="right: 1.2rem;bottom: 1rem;">
         <i class="fa fa-2x fa-arrow-up"></i>
     </a>
+    <div class="fixed-bottom container">
+        <p class="text-center text-muted" style="font-size: 75%;">
+            <span>本网站受 reCAPTCHA 保护，并适用Google</span>
+            <a class="text-reset text-decoration-none" href="https://www.google.cn/intl/zh-CN/policies/privacy/"
+               target="_blank">隐私权</a>
+            <span>和</span>
+            <a class="text-reset text-decoration-none" href="https://www.google.cn/intl/zh-CN/policies/terms/"
+               target="_blank">使用条款</a>
+            <span>。</span>
+        </p>
+    </div>
 </div>
 <div>
     <script src="/tools/static/js/jquery.min.js"></script>
