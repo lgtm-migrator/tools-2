9b55a4ccdfad26db385e2990ae49132e2f2446c3
zhangchenglin <469946668@qq.com>
2019-11-13 19:43:25 +0800

    Initial commit

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
         <i class="fa fa-2x fa-arrow-up"></i>
     </a>
     <div class="container" id="recaptcha_text_badge">
-        <p class="text-center text-muted" style="font-size: 75%;">
-            <span>本网站由 reCAPTCHA 提供保护，并适用Google</span>
+        <p class="text-nowrap text-center text-muted" style="font-size: 75%;">
+            <span>本站由 reCAPTCHA 提供保护，并适用Google</span>
             <a class="text-reset text-decoration-none" href="https://www.google.cn/intl/zh-CN/policies/privacy/"
                target="_blank">隐私权</a>
             <span>和</span>
