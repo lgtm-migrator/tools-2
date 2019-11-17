caae0228ec313b1e65f69a12c364f5a1d7870c5e
zhangchenglin <469946668@qq.com>
2019-11-17 23:22:32 +0800

    Initial commit

--- recaptcha/recaptcha_verify_v3.php
+++ recaptcha/recaptcha_verify_v3.php
@@ -64,23 +64,33 @@ function recaptcha_data_to_database($resp_array)
     require_once "../mysqli/config.php";
 
     $udate = new DateTime();
-    $created_time = $udate->format("Y-m-d H:i:s.u");
 
+
+    $action = $resp_array['action'];
+    $hostname = $resp_array['hostname'];
+    $score = $resp_array['score'];
+    $error_codes = json_encode($resp_array['error-codes']);
+    $challenge_ts = $resp_array['challenge_ts'];
+    $challenge_ts_time = return_YmdHisu(challenge_ts_to_dataTime($challenge_ts));
+    $created_time = $udate->format("Y-m-d H:i:s.u");
+    $category = "";
+    $apk_package_name = $resp_array['apk_package_name'];
     $user_agent = filter_input(INPUT_SERVER, "HTTP_USER_AGENT");
 
     $google_recaptcha_data = array(
-        "action" => $resp_array['action'],
-        "hostname" => $resp_array['hostname'],
-        "challenge_ts" => $resp_array['challenge_ts'],
-        "score" => $resp_array['score'],
-        "error_codes" => json_encode($resp_array['error-codes']),
-        "apk_package_name" => $resp_array['apk_package_name'],
+        "action" => $action,
+        "hostname" => $hostname,
+        "score" => $score,
         "threshold" => $threshold,
         "remote_ip" => $remoteIp,
+        "error_codes" => $error_codes,
         "timeout_seconds" => $timeoutSeconds,
-        "user_agent" => $user_agent,
+        "challenge_ts" => $challenge_ts,
+        "challenge_ts_time" => $challenge_ts_time,
         "created_time" => $created_time,
-        "category" => "",
+        "category" => $category,
+        "apk_package_name" => $apk_package_name,
+        "user_agent" => $user_agent,
     );
 
 
@@ -120,4 +130,29 @@ function utc_time()
     $timestamp = new DateTime();
     $timeStr = $timestamp->format("Y-m-d\TH:i:s\Z");
     return $timeStr;
+}
+
+function challenge_ts_to_dataTime($challenge_ts)
+{
+    $challenge_ts = is_string($challenge_ts) ? $challenge_ts : (string)$challenge_ts;
+    $patterns = array(
+        '/T/',
+        '/Z$/'
+    );
+    $replace = array(
+        ' ',
+        ''
+    );
+    $challenge_ts = preg_replace($patterns, $replace, $challenge_ts, 1);
+    try {
+        $new_dateTime = new DateTime($challenge_ts);
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
+    return $new_dateTime;
+}
+
+function return_YmdHisu($dataTime)
+{
+    return $dataTime->format("Y-m-d H:i:s.u");
 }
--- recaptcha/sql/google_recaptcha_data.sql
+++ recaptcha/sql/google_recaptcha_data.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地数据库8.0
+ Source Server         : aliyun
  Source Server Type    : MySQL
  Source Server Version : 80018
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 14/11/2019 00:00:00
+ Date: 17/11/2019 00:00:00
 */
 
 SET NAMES utf8mb4;
@@ -31,11 +31,12 @@ CREATE TABLE `google_recaptcha_data`  (
   `error_codes` json NOT NULL COMMENT '错误代码',
   `timeout_seconds` int(4) NOT NULL COMMENT '超时时间值',
   `challenge_ts` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '时间',
+  `challenge_ts_time` datetime(6) NOT NULL COMMENT '本地化 challenge_ts',
   `created_time` datetime(6) NOT NULL COMMENT '创建记录时间',
   `category` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '分类',
   `apk_package_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '安卓包名',
   `user_agent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户UA',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'Google ReCAPTCHA 触发失败时记录值' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'Google ReCAPTCHA 触发失败时记录值' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
