b4bf61a239ba2054f58ee58da9d2d7f2b29ee969
zhangchenglin <469946668@qq.com>
2019-11-17 14:58:22 +0800

    Initial commit

--- photo_info/js/photo_info.js
+++ photo_info/js/photo_info.js
@@ -8,11 +8,11 @@ let photo_submit = document.querySelector("#photo_submit");
 let photo_input = document.querySelector("#photo_input");
 let photo_url = "/tools/photo_info/photoinfo.php";
 
-photo_submit.addEventListener("click", get_images);
+photo_submit.addEventListener("click", submit_images);
 
 function get_images() {
-    const images = photo_input.value;
-    console.log(images);
+    const images = photo_input.files;
+    console.table(images);
 }
 
 function submit_images() {
@@ -20,15 +20,18 @@ function submit_images() {
         url: photo_url,
         method: "post",
         cache: false,
+        timeout: 10000,
         contentType: "multipart/form-data",
-        data:"",
+        data: get_images(),
         beforeSend: function () {
 
         },
         success: function (data) {
-            console.log(data);
+            console.log("成功");
+            console.log(data.result);
         },
         error: function (error) {
+            console.log("失败");
             console.log(error);
         }
     })
--- tools.js
+++ tools.js
@@ -1,4 +1,161 @@
 
+/** 公用 **/
+
+function validation_invalid_div(e, text, type = "tooltip") {
+    let e_target = e.target;
+    if (!e_target.nextElementSibling) {
+        let div = document.createElement("div");
+        if (type === "tooltip") {
+            div.className = "invalid-tooltip";
+            div.style.position = "unset";
+        } else {
+            div.className = "invalid-feedback";
+        }
+        div.innerText = text;
+        e_target.parentNode.appendChild(div);
+    }
+}
+
+function validation_valid_div(e, text, type = "tooltip") {
+    let e_target = e.target;
+    if (!e_target.nextElementSibling) {
+        let div = document.createElement("div");
+        if (type === "tooltip") {
+            div.className = "valid-tooltip";
+            div.style.position = "unset";
+        } else {
+            div.className = "valid-feedback";
+        }
+        div.innerText = text;
+        e_target.parentNode.appendChild(div);
+    }
+}
+
+function remove_validation_div(e) {
+    let e_target = e.target;
+    while (e_target.nextElementSibling) {
+        e_target.nextElementSibling.remove();
+    }
+}
+
+function input_error(e) {
+    let e_target = e.target;
+    e_target.classList.remove("is-valid");
+    e_target.classList.add("is-invalid");
+}
+
+function input_success(e) {
+    let e_target = e.target;
+    e_target.classList.remove("is-invalid");
+    e_target.classList.add("is-valid");
+}
+
+function add_spinner_icon(e, spinner_type = null, color = null, position = null) {
+    let load_icon = document.createElement("span");
+    switch (spinner_type) {
+        case "grow":
+            load_icon.className = "ml-1 spinner-grow spinner-grow-sm align-self-center";
+            break;
+        case "border":
+        default:
+            load_icon.className = "ml-1 spinner-border spinner-border-sm align-self-center";
+    }
+
+    switch (color) {
+        case 'primary':
+            load_icon.classList.add("text-primary");
+            break;
+        case "secondary":
+            load_icon.classList.add("text-secondary");
+            break;
+        case "success":
+            load_icon.classList.add("text-success");
+            break;
+        case "danger":
+            load_icon.classList.add("text-danger");
+            break;
+        case "warning":
+            load_icon.classList.add("text-warning");
+            break;
+        case "info":
+            load_icon.classList.add("text-info");
+            break;
+        case "light":
+            load_icon.classList.add("text-light");
+            break;
+        case "dark":
+            load_icon.classList.add("text-dark");
+            break;
+        default:
+            break;
+    }
+
+    e.setAttribute("disabled", "disabled");
+
+    switch (position) {
+        case "before":
+        case "left":
+            e.insertBefore(load_icon, e.firstChild);
+            break;
+        case "after":
+        case "right":
+        default:
+            e.appendChild(load_icon);
+    }
+}
+
+function remove_spinner_icon(e) {
+    e.removeAttribute("disabled");
+    e.firstElementChild ? e.removeChild(e.firstElementChild) : "";
+}
+
+
+/** ReCAPTCHA **/
+function set_recaptcha_action(Action = null) {
+    const v3_site_key = "6LcvIcEUAAAAAEUgtbN0VFiH_n2VHw-luW3rdZFv";
+    const url = "https://www.recaptcha.net/recaptcha/api.js?render=";
+
+    Action = Action ? Action : "unset";
+
+    $.getScript(url + v3_site_key, function () {
+        set_recaptcha_token(v3_site_key, Action);
+    });
+}
+
+function set_recaptcha_token(site_key, action) {
+    grecaptcha.ready(function () {
+        grecaptcha.execute(site_key, {action: action})
+            .then(function (token) {
+                // console.log(token);
+                get_recaptcha_verify(token, action);
+            });
+    });
+}
+
+function get_recaptcha_verify(token_key, pageAction) {
+    let url = "/tools/recaptcha/recaptcha_verify_v3.php";
+    let data = {
+        token: token_key,
+        action: pageAction,
+    };
+
+    $.ajax({
+        type: "post",
+        url: url,
+        data: data,
+        timeout: 5000,
+        dataType: "json",
+        success: function (data) {
+            console.log("提交验证成功");
+            console.log(data);
+        },
+        error: function (data) {
+            console.log("提交验证失败");
+            console.log(data);
+        }
+    })
+}
+
 /** 增加阴影 **/
 let btn_all = document.querySelectorAll("[class*='btn']");
 let input_all = document.querySelectorAll("input[class*='form-control']");
