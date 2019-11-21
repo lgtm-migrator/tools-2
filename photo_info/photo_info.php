24106aa8caec53d6c2ff9314371a5f89cdfe0326
zhangchenglin <469946668@qq.com>
2019-11-21 20:34:47 +0800

    Initial commit

--- photo_info/photo_info.php
+++ photo_info/photo_info.php
@@ -1,7 +1,8 @@
 <?php
 if ($_POST) {
     if (isset($_POST['MAX_FILE_SIZE']) && $_FILES !== null) {
-        echo json_encode($_FILES) . "<br><br>" . json_encode($_POST);
+        $image_files = $_FILES['photo_input'];
+        if (count($image_files['name']) > 4) die("请上传少于4个文件");
     } else {
         die("数据错误");
     }
@@ -11,13 +12,12 @@ if ($_POST) {
 
 require_once dirname(dirname(__FILE__)) . "/defined.php";
 
-$image_files = $_FILES['photo_input'];
-
-echo "<br>";
-echo "<br>";
-//echo json_encode($_FILES);
-echo "<br>";
-echo "<br>";
+//echo "<br>";
+//echo "<br>";
+////echo json_encode($_FILES);
+////echo count($image_files['name']);
+//echo "<br>";
+//echo "<br>";
 
 
 $allowed_extension_name = array("jfif", "pjpeg", "jpeg", "pjp", "jpg", "tiff", "tif");
@@ -31,19 +31,20 @@ foreach ($image_files["error"] as $k => $error) {
         $name_file = $image_files['name'][$k];
         if (is_uploaded_file($tmp_file)) {
             if (!file_exists(UPLOAD_DIR_YMD . $name_file)) {
-                mk_dir(UPLOAD_DIR_YMD);
+                if (!file_exists(UPLOAD_DIR_YMD)) mk_dir(UPLOAD_DIR_YMD);
                 move_uploaded_file($tmp_file, UPLOAD_DIR_YMD . $name_file);
             } else {
-                echo '您上传的文件 ' . $name_file . ' 已经存在' . "<br>";
+                echo '您上传的文件 ' . $name_file . ' 已经存在' . "\n";
             }
         }
     }
 }
 
+
 //
 function mk_dir($pathname)
 {
-    return mkdir($pathname, 0775, true);
+    mkdir($pathname, 0775, true);
 }
 
 //https://www.php.net/manual/zh/features.file-upload.errors.php
