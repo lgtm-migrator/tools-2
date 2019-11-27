983504feee3b9d04ca82d5a3a4cc595f5ca8b811
zhangchenglin <469946668@qq.com>
2019-11-27 10:47:41 +0800

    Initial commit

--- photo_info/photo_info_fun.php
+++ photo_info/photo_info_fun.php
@@ -18,3 +18,35 @@ function get_file_ext_name($file_name)
 function file_error_status()
 {
 }
+
+function filters_image_types($file_name, $file_ext_name, $tmp_file_name)
+{
+    $php_created_image = PHP_TMP_DIR . $file_name;
+
+    if ($file_ext_name === 'jpg' || $file_ext_name === 'jpeg' && (imagetypes() & IMG_JPG)) {
+        $image_tmp = imagecreatefromjpeg($tmp_file_name);
+        $image_create_result = imagejpeg($image_tmp, $php_created_image, 100);
+        imagedestroy($image_tmp);
+    } elseif ($file_ext_name === 'tiff' || $file_ext_name === 'tif') {
+        //todo:处理tiff格式
+        $image_tmp = imagecreatefromjpeg($tmp_file_name);
+        $image_create_result = imagejpeg($image_tmp, $php_created_image, 100);
+        imagedestroy($image_tmp);
+    } elseif ($file_ext_name === 'png' && (imagetypes() & IMG_PNG)) {
+        $image_tmp = imagecreatefrompng($tmp_file_name);
+        $image_create_result = imagepng($image_tmp, $php_created_image, 9);
+        imagedestroy($image_tmp);
+    } elseif ($file_ext_name === 'gif' && (imagetypes() & IMG_GIF)) {
+        $image_tmp = imagecreatefromgif($tmp_file_name);
+        $image_create_result = imagegif($image_tmp, $php_created_image);
+        imagedestroy($image_tmp);
+    } elseif ($file_ext_name === 'webp' && (imagetypes() & IMG_WEBP)) {
+        $image_tmp = imagecreatefromwebp($tmp_file_name);
+        $image_create_result = imagewebp($image_tmp, $php_created_image, 100);
+        imagedestroy($image_tmp);
+    } else {
+        $image_create_result = false;
+    }
+
+    return $image_create_result;
+}
