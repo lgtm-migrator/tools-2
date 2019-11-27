efb7ce12f7a22bf573d78982678fabe217d03a34
zhangchenglin <469946668@qq.com>
2019-11-27 15:53:13 +0800

    Initial commit

--- photo_info/js/photo_info.js
+++ photo_info/js/photo_info.js
@@ -131,7 +131,7 @@ function upload_files_check(input) {
         bootstrapModalJs("", upload_check_result, "", "", true);
     }
 
-    if (files_size_tips.length === 0 && disallow_files.length === 0) {
+    if (files_size_tips.length === 0 && disallow_files.length === 0 && files_length > 0) {
         ajax_images();
     }
 
