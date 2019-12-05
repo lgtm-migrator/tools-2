11e651c704e48776d72891e89e1ca6a9fd8bd942
zhangchenglin <469946668@qq.com>
2019-12-05 20:14:59 +0800

    调整 列表样式

--- index.php
+++ index.php
@@ -5,23 +5,23 @@ require_once './header.php';
     <div class="container" id="jt_index">
         <div id="jt_category"></div>
 
-        <div class="d-flex flex-wrap" id="jt_list">
-            <div class="card mr-2 mb-2">
+        <div class="d-flex flex-wrap no-gutters" id="jt_list">
+            <div class="col-12 col-md-8 col-lg-3 card mr-2 mb-3">
                 <div class="d-flex justify-content-between card-header px-4 py-2">
                     <a class="text-success font-weight-bold" id="phoneNumber" href="/phone_number/index.php"
-                       title="公共电话本">公共电话本</a>
-                    <div class="category text-muted">[&nbsp;查询&nbsp;]</div>
+                       title="公共电话本" style="font-size: 95%;">公共电话本</a>
+                    <div class="category text-muted small">[&nbsp;查询&nbsp;]</div>
                 </div>
-                <p class="card-desc card-text p-2">公开的电话号码本。</p>
+                <p class="card-desc card-text p-2 small">公开的电话号码本。</p>
             </div>
 
-            <div class="card mr-2 mb-2">
+            <div class="col-12 col-md-8 col-lg-3 card mr-2 mb-3">
                 <div class="d-flex justify-content-between card-header px-4 py-2">
                     <a class="text-success font-weight-bold" id="phoneNumber" href="/photo_info/index.php"
-                       title="照片详情信息">照片详情信息</a>
-                    <div class="category text-muted">[&nbsp;查询&nbsp;]</div>
+                       title="照片详情信息" style="font-size: 95%;">照片详情信息</a>
+                    <div class="category text-muted small">[&nbsp;查询&nbsp;]</div>
                 </div>
-                <p class="card-desc card-text p-2">查看照片的EXIF信息。</p>
+                <p class="card-desc card-text p-2 small">查看照片的EXIF信息。</p>
             </div>
 
         </div>
