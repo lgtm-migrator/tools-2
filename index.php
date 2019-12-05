1a438f611cd8062b7c7aacb799f5b36ccb58236f
zhangchenglin <469946668@qq.com>
2019-12-05 21:47:13 +0800

    调整 列表样式

--- index.php
+++ index.php
@@ -5,23 +5,31 @@ require_once './header.php';
     <div class="container" id="jt_index">
         <div id="jt_category"></div>
 
-        <div class="d-flex flex-wrap no-gutters" id="jt_list">
-            <div class="col-12 col-md-8 col-lg-3 card mr-2 mb-3">
-                <div class="d-flex justify-content-between card-header px-4 py-2">
-                    <a class="text-success font-weight-bold" id="phoneNumber" href="/phone_number/index.php"
-                       title="公共电话本" style="font-size: 95%;">公共电话本</a>
-                    <div class="category text-muted small">[&nbsp;查询&nbsp;]</div>
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4" id="jt_list">
+            <div class="col mb-3">
+                <div class="card">
+                    <div class="d-flex justify-content-between card-header py-2">
+                        <a class="text-success font-weight-bold" id="phoneNumber" href="/phone_number/index.php"
+                           title="公共电话本" style="font-size: 95%;">公共电话本</a>
+                        <div class="category text-muted small">[&nbsp;查询&nbsp;]</div>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-desc card-text small">公开的电话号码本。</p>
+                    </div>
                 </div>
-                <p class="card-desc card-text p-2 small">公开的电话号码本。</p>
             </div>
 
-            <div class="col-12 col-md-8 col-lg-3 card mr-2 mb-3">
-                <div class="d-flex justify-content-between card-header px-4 py-2">
-                    <a class="text-success font-weight-bold" id="phoneNumber" href="/photo_info/index.php"
-                       title="照片详情信息" style="font-size: 95%;">照片详情信息</a>
-                    <div class="category text-muted small">[&nbsp;查询&nbsp;]</div>
+            <div class="col mb-3">
+                <div class="card">
+                    <div class="d-flex justify-content-between card-header py-2">
+                        <a class="text-success font-weight-bold" id="phoneNumber" href="/photo_info/index.php"
+                           title="照片详情信息" style="font-size: 95%;">照片详情信息</a>
+                        <div class="category text-muted small">[&nbsp;查询&nbsp;]</div>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-desc card-text small">查看照片的EXIF信息。</p>
+                    </div>
                 </div>
-                <p class="card-desc card-text p-2 small">查看照片的EXIF信息。</p>
             </div>
 
         </div>
