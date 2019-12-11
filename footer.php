4bc3a2bc1382bf6818fbd437a3073198742243a4
zhangchenglin <469946668@qq.com>
2019-12-11 09:35:17 +0800

    调整 页脚样式

--- footer.php
+++ footer.php
@@ -1,13 +1,14 @@
 <?php
 ?>
 </div>
-<div class="mt-5 mb-2" id="jt_footer">
+<div class="mt-3 mt-lg-4 pb-4 bg-dark" id="jt_footer">
+    <div class="my-3 border-bottom"></div>
     <a class="position-fixed border border-primary rounded p-1" href="javascript:" id="to_top"
        style="right: 1.2rem;bottom: 1rem;">
         <i class="fa fa-2x fa-arrow-up"></i>
     </a>
-    <div class="container" style="font-size: 75%;">
-        <span class="my-1 d-block text-nowrap text-center text-muted" id="recaptcha_text_badge">
+    <div class="container small">
+        <span class="my-2 d-block text-nowrap text-center text-white-50" id="recaptcha_text_badge">
             <span>由 reCAPTCHA 提供保护，并适用Google</span>
             <a class="text-reset text-decoration-none" href="https://www.google.cn/intl/zh-CN/policies/privacy/"
                target="_blank">隐私权</a>
@@ -15,7 +16,7 @@
             <a class="text-reset text-decoration-none" href="https://www.google.cn/intl/zh-CN/policies/terms/"
                target="_blank">使用条款</a>
         </span>
-        <span class="my-1 d-block text-nowrap text-center text-muted" id="moment_time"></span>
+        <span class="my-2 d-block text-nowrap text-center text-white-50" id="moment_time"></span>
     </div>
 </div>
 <div>
