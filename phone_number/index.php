014218236c1b6ed8524f21c8a9bc847c31169d4e
zhangchenglin <469946668@qq.com>
2020-01-09 21:27:06 +0800

    调整 简单调整样式

--- phone_number/index.php
+++ phone_number/index.php
@@ -37,21 +37,21 @@ require_once '../header.php';
 
     <div class="container mt-5">
         <div class="px-4 py-3 border border-info rounded fade d-none" id="add_phone_number_form">
-            <div class="container mb-5 mb-sm-4 mb-md-3" id="regional">
-                <div class="custom-control custom-radio custom-control-inline">
+            <div class="d-flex container justify-content-center px-0 mb-5 mb-sm-4 mb-md-3 row-cols-4" id="regional">
+                <div class="custom-control custom-radio">
                     <input type="radio" id="regional_xm" name="regional" value="xm" class="custom-control-input"
                            checked>
                     <label class="custom-control-label" for="regional_xm">邢煤</label>
                 </div>
-                <div class="custom-control custom-radio custom-control-inline">
+                <div class="custom-control custom-radio">
                     <input type="radio" id="regional_gq" name="regional" value="gq" class="custom-control-input">
                     <label class="custom-control-label" for="regional_gq">葛泉</label>
                 </div>
-                <div class="custom-control custom-radio custom-control-inline">
+                <div class="custom-control custom-radio">
                     <input type="radio" id="regional_dp" name="regional" value="dp" class="custom-control-input">
                     <label class="custom-control-label" for="regional_dp">东庞</label>
                 </div>
-                <div class="custom-control custom-radio custom-control-inline">
+                <div class="custom-control custom-radio">
                     <input type="radio" id="regional_xdw" name="regional" value="xdw" class="custom-control-input">
                     <label class="custom-control-label" for="regional_xdw">显德旺</label>
                 </div>
