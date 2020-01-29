3e90ad366359d3fb08e9bcc5b67fc437b9596e27
zhangchenglin <469946668@qq.com>
2020-01-29 17:14:55 +0800

    调整 添加公共元素变量名

--- tools_static_src/js/tools.js
+++ tools_static_src/js/tools.js
@@ -13,6 +13,11 @@ let RegExp_rules = {
     'zh_cn_number': new RegExp(/^((?:[\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\uD840-\uD868\uD86A-\uD86C\uD86F-\uD872\uD874-\uD879][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D\uDC20-\uDFFF]|\uD873[\uDC00-\uDEA1\uDEB0-\uDFFF]|\uD87A[\uDC00-\uDFE0])|(\d))+$/),
 };
 
+/** 公共元素变量名 **/
+let jt_header = document.querySelector('#jt_header');
+let jt_content = document.querySelector('#jt_content');
+let jt_footer = document.querySelector('#jt_footer');
+
 /** 封装方法 **/
 function add_class(element, class_name) {
     element.classList.add(class_name);
