615284a58d63cfe0f9e7196ebc2b9243ec7b3cb7
zhangchenglin <469946668@qq.com>
2020-02-27 22:16:22 +0800

    初始化杂项调整

--- qrcode/img.php
+++ qrcode/img.php
@@ -1,16 +1,7 @@
 <?php
 
 if ($_POST && $_FILES) {
-    if ($_FILES['qrcode_img'] && $_POST['qrcode_title'] && $_POST['qrcode_description'] && $_POST['qrcode_password']) {
-        global $new_img_file, $result;
-
-        $qrcode_img_file = $_FILES['qrcode_img'];
-        require_once "./file.php";
-
-        $qrcode_title = $_POST['qrcode_title'];
-        $qrcode_description = $_POST['qrcode_description'];
-        $qrcode_password = $_POST['qrcode_password'];
-    } else {
+    if (!($_FILES['qrcode_img']) || !($_POST['qrcode_title']) || !($_POST['qrcode_description']) || !($_POST['qrcode_password'])) {
         die('参数错误');
     }
 } else {
@@ -18,12 +9,23 @@ if ($_POST && $_FILES) {
 }
 
 
+global $new_img_file, $result;
+
+$qrcode_img_file = $_FILES['qrcode_img'];
+require_once "./file.php";
+
+
+$qrcode_title = $_POST['qrcode_title'];
+$qrcode_description = $_POST['qrcode_description'];
+$qrcode_password = $_POST['qrcode_password'];
+
 $img_path = $new_img_file['file_path'];
 $title = $qrcode_title;
 $description = $qrcode_description;
 $password = $qrcode_password;
 $category = 'img_qrcode';
 $identifier = 'LHM-' . time();
+//$identifier = 'LHM-1582811959';
 $img_md5 = '111';
 $qrcode_content = '111';
 $static = 'yes';
