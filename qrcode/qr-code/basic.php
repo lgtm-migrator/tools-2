57fbbafb28f73ecba2e6c4ce5826578711b6243a
zhangchenglin <469946668@qq.com>
2020-02-27 22:06:17 +0800

    调整 使用方式

--- qrcode/qr-code/basic.php
+++ qrcode/qr-code/basic.php
@@ -1,4 +1,6 @@
 <?php
+if (!defined('JT_QRCODE')) die();
+
 require_once dirname(dirname(__DIR__)) . "/vendor/autoload.php";
 
 use Endroid\QrCode\ErrorCorrectionLevel;
@@ -9,93 +11,65 @@ if (!function_exists('create_qrcode')) {
     function create_qrcode(string $text, array $options = null, string $writeFile = null)
     {
         if (null === $text) return false;
-
-        if (null === $options) {
-            $options = array(
-                'Size' => 300,
-                'WriterByName' => 'png',
-                'Margin' => 5,
-                'Encoding' => 'UTF-8',
-                'ErrorCorrectionLevel' => ErrorCorrectionLevel::HIGH(),
-                'ForegroundColor' => array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0),
-                'BackgroundColor' => array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0),
-                'LabelContent' => '灵活码',
-                'LabelFontSize' => 14,
-                'LabelFontPath' => __DIR__ . '/assets/fonts/msyhl.ttc',
-                'LabelAlignment' => LabelAlignment::CENTER(),
-                'LabelMargin' => array('t' => 0, 'r' => 10, 'b' => 5, 'l' => 10,),
-                'LogoPath' => __DIR__ . '/assets/images/g.jpg',
-                'LogoWidth' => 50,
-                'LogoHeight' => 50,
-                'RoundBlockSize' => true,
-                'ValidateResult' => false,
-                'WriterOptions' => array('exclude_xml_declaration' => true),
-            );
+        $default_options = array(
+            'Size' => 300,
+            'WriterByName' => 'png',
+            'Margin' => 5,
+            'Encoding' => 'UTF-8',
+            'ErrorCorrectionLevel' => ErrorCorrectionLevel::HIGH(),
+            'ForegroundColor' => array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0),
+            'BackgroundColor' => array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0),
+            'LabelContent' => '灵活码',
+            'LabelFontSize' => 14,
+            'LabelFontPath' => __DIR__ . '/assets/fonts/Deng.ttf',
+            'LabelAlignment' => LabelAlignment::CENTER(),
+            'LabelMargin' => array('t' => 0, 'r' => 10, 'b' => 10, 'l' => 10,),
+            'LogoPath' => __DIR__ . '/assets/images/logo.jpg',
+            'LogoWidth' => 50,
+            'LogoHeight' => 50,
+            'RoundBlockSize' => true,
+            'ValidateResult' => false,
+            'WriterOptions' => array('exclude_xml_declaration' => true),
+        );
+        if (null !== $options) {
+            $new_options = array_replace($default_options, $options);
+        } else {
+            $new_options = $default_options;
         }
 
-        // 创建基本的QR码
         $qrCode = new QrCode($text);
-        $qrCode->setSize($options['Size']);
-
-        // 设定进阶选项
-        $qrCode->setWriterByName($options['WriterByName']);
-        $qrCode->setMargin($options['Margin']);
-        $qrCode->setEncoding($options['Encoding']);
-        $qrCode->setErrorCorrectionLevel($options['ErrorCorrectionLevel']);
-        $qrCode->setForegroundColor($options['ForegroundColor']);
-        $qrCode->setBackgroundColor($options['BackgroundColor']);
-        $qrCode->setLabel($options['LabelContent'], $options['LabelFontSize'], $options['LabelFontPath'], $options['LabelAlignment'], $options['LabelMargin']);
-
+        $qrCode->setSize($new_options['Size']);
+        $qrCode->setWriterByName($new_options['WriterByName']);
+        $qrCode->setMargin($new_options['Margin']);
+        $qrCode->setEncoding($new_options['Encoding']);
+        $qrCode->setErrorCorrectionLevel($new_options['ErrorCorrectionLevel']);
+        $qrCode->setForegroundColor($new_options['ForegroundColor']);
+        $qrCode->setBackgroundColor($new_options['BackgroundColor']);
+        $qrCode->setLabel($new_options['LabelContent'], $new_options['LabelFontSize'], $new_options['LabelFontPath'], $new_options['LabelAlignment'], $new_options['LabelMargin']);
         try {
-            $qrCode->setLogoPath($options['LogoPath']);
+            $qrCode->setLogoPath($new_options['LogoPath']);
         } catch (Endroid\QrCode\Exception\InvalidPathException $e) {
             die($e->getMessage());
         }
-        $qrCode->setLogoSize($options['LogoWidth'], $options['LogoHeight']);
+        $qrCode->setLogoSize($new_options['LogoWidth'], $new_options['LogoHeight']);
+        $qrCode->setRoundBlockSize($new_options['RoundBlockSize']);
+        $qrCode->setValidateResult($new_options['ValidateResult']);
+        $qrCode->setWriterOptions($new_options['WriterOptions']);
 
-        $qrCode->setRoundBlockSize($options['RoundBlockSize']);
-        $qrCode->setValidateResult($options['ValidateResult']);
-        $qrCode->setWriterOptions($options['WriterOptions']);
-
-//        保存到文件
+        // 保存到文件
         if (null !== $writeFile) {
             $qrCode->writeFile($writeFile);
         }
 
-//        创建一个响应对象
-//        $response = new QrCodeResponse($qrCode);
-//        if (null !== $QrCodeResponse && true === $QrCodeResponse) {
-//            return new QrCodeResponse($qrCode);
-//        }
+        // 创建一个响应对象
+        //$response = new QrCodeResponse($qrCode);
+        //if (null !== $QrCodeResponse && true === $QrCodeResponse) {
+        //return new QrCodeResponse($qrCode);
+        //}
 
-//        直接输出二维码
+        // 直接输出二维码
         header('Content-Type: ' . $qrCode->getContentType());
         return $qrCode->writeString();
 
     }
 }
-
-$options = array(
-    'Size' => 300,
-    'WriterByName' => 'png',
-    'Margin' => 5,
-    'Encoding' => 'UTF-8',
-    'ErrorCorrectionLevel' => ErrorCorrectionLevel::HIGH(),
-    'ForegroundColor' => array('r' => 255, 'g' => 0, 'b' => 0, 'a' => 0),
-    'BackgroundColor' => array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0),
-    'LabelContent' => '灵活码',
-    'LabelFontSize' => 14,
-    'LabelFontPath' => __DIR__ . '/assets/fonts/msyhl.ttc',
-    'LabelAlignment' => LabelAlignment::CENTER(),
-    'LabelMargin' => array('t' => 0, 'r' => 10, 'b' => 5, 'l' => 10,),
-    'LogoPath' => __DIR__ . '/assets/images/g.jpg',
-    'LogoWidth' => 50,
-    'LogoHeight' => 50,
-    'RoundBlockSize' => true,
-    'ValidateResult' => false,
-    'WriterOptions' => array('exclude_xml_declaration' => true),
-);
-
-$save_file_path = '../../upload/qrcode_img/qrcode.png';
-//echo $save_file_path;
-echo create_qrcode('00000', $options, $save_file_path);
---
+++ qrcode/qr-code/index.php
@@ -0,0 +1,33 @@
+<?php
+if (!defined('JT_QRCODE')) define('JT_QRCODE', true);
+
+require_once "./basic.php";
+
+$options = array(
+    'Size' => 300,
+    'WriterByName' => 'png',
+    'Margin' => 5,
+    'Encoding' => 'UTF-8',
+//    'ErrorCorrectionLevel' => ErrorCorrectionLevel::HIGH(),
+    'ForegroundColor' => array('r' => 255, 'g' => 0, 'b' => 0, 'a' => 0),
+    'BackgroundColor' => array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0),
+    'LabelContent' => '本灵活码可维护使用',
+    'LabelFontSize' => 14,
+    'LabelFontPath' => __DIR__ . '/assets/fonts/Deng.ttf',
+//    'LabelAlignment' => LabelAlignment::CENTER(),
+    'LabelAlignment' => 'center',
+    'LabelMargin' => array('t' => 0, 'r' => 10, 'b' => 4, 'l' => 10,),
+    'LogoPath' => __DIR__ . '/assets/images/logo.jpg',
+    'LogoWidth' => 50,
+    'LogoHeight' => 50,
+    'RoundBlockSize' => true,
+    'ValidateResult' => false,
+    'WriterOptions' => array('exclude_xml_declaration' => true),
+);
+
+
+$qrcode_url_text = 'http://tools.jzeg.org/qrcode/q.php?q=dd';
+$save_file_path = '../../upload/qrcode_img/qrcode.png';
+
+//echo create_qrcode($qrcode_url_text, $options, $save_file_path);
+echo create_qrcode($qrcode_url_text, $options);
