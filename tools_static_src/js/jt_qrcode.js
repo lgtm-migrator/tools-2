84363cf56dce2fe1c0787f0a60a825b1d43768fb
zhangchenglin <469946668@qq.com>
2020-03-06 22:09:20 +0800

    调整 动态同步文本输入框元素和滑动条元素的值

--- tools_static_src/js/jt_qrcode.js
+++ tools_static_src/js/jt_qrcode.js
@@ -15,33 +15,33 @@ $().ready(function () {
     }
 });
 
+/**
+ * 动态同步文本输入框元素和滑动条元素的值
+ */
 $().ready(function () {
     let lhm_editor = document.querySelector('#lhm_editor');
-    let lhm_editor_size_range = document.querySelector('#lhm_editor_size_range');
     let lhm_editor_size_value = document.querySelector('#lhm_editor_size_value');
+    let lhm_editor_size_range = document.querySelector('#lhm_editor_size_range');
     let lhm_editor_quality_value = document.querySelector('#lhm_editor_quality_value');
     let lhm_editor_quality_range = document.querySelector('#lhm_editor_quality_range');
     let lhm_editor_margin_value = document.querySelector('#lhm_editor_margin_value');
     let lhm_editor_margin_range = document.querySelector('#lhm_editor_margin_range');
+    let lhm_editor_xxx_elements = [
+        [lhm_editor_size_value, lhm_editor_size_range],
+        [lhm_editor_quality_value, lhm_editor_quality_range],
+        [lhm_editor_margin_value, lhm_editor_margin_range],
+    ];
 
     if (lhm_editor) {
-        lhm_editor_size_value.addEventListener('input', function () {
-            lhm_editor_size_range.value = lhm_editor_size_value.value;
-        });
-        lhm_editor_size_range.addEventListener('input', function () {
-            lhm_editor_size_value.value = lhm_editor_size_range.value;
-        });
-        lhm_editor_quality_value.addEventListener('input', function () {
-            lhm_editor_quality_range.value = lhm_editor_quality_value.value;
-        });
-        lhm_editor_quality_range.addEventListener('input', function () {
-            lhm_editor_quality_value.value = lhm_editor_quality_range.value;
-        });
-        lhm_editor_margin_value.addEventListener('input', function () {
-            lhm_editor_margin_range.value = lhm_editor_margin_value.value;
-        });
-        lhm_editor_margin_range.addEventListener('input', function () {
-            lhm_editor_margin_value.value = lhm_editor_margin_range.value;
-        });
+        for (let x = lhm_editor_xxx_elements.length, i = 0; i < x; i++) {
+            dynamic_synchronization_element(lhm_editor_xxx_elements[i]['0'], lhm_editor_xxx_elements[i]['1'], 'input');
+            dynamic_synchronization_element(lhm_editor_xxx_elements[i]['1'], lhm_editor_xxx_elements[i]['0'], 'input');
+        }
+    }
+
+    function dynamic_synchronization_element(source_element, target_element, event_type) {
+        source_element.addEventListener(event_type, function () {
+            target_element.value = source_element.value;
+        })
     }
 });
