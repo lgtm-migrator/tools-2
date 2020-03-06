bbd88279bc684b51fc3c4e4cfe56d40281d2be67
zhangchenglin <469946668@qq.com>
2020-03-06 23:52:58 +0800

    添加 初始化 监控编辑器生成的图片类型

--- tools_static_src/js/jt_qrcode.js
+++ tools_static_src/js/jt_qrcode.js
@@ -40,3 +40,48 @@ $().ready(function () {
     }
 
 });
+
+/**
+ * 监控编辑器生成的图片类型
+ * 根据图片类型调整图片质量的数值和数值范围
+ */
+$().ready(function () {
+    let lhm_editor_img_format = document.querySelector('#lhm_editor_img_format');
+    let lhm_editor_quality_value = document.querySelector('#lhm_editor_quality_value');
+    let lhm_editor_quality_value_min = lhm_editor_quality_value.getAttribute('min');
+    let lhm_editor_quality_value_max = lhm_editor_quality_value.getAttribute('max');
+    let lhm_editor_quality_value_step = lhm_editor_quality_value.getAttribute('step');
+    let lhm_editor_quality_range = document.querySelector('#lhm_editor_quality_range');
+    let lhm_editor_quality_range_min = lhm_editor_quality_range.getAttribute('min');
+    let lhm_editor_quality_range_max = lhm_editor_quality_range.getAttribute('max');
+    let lhm_editor_quality_range_step = lhm_editor_quality_range.getAttribute('step');
+
+    // let type_array = [];
+    // type_array.push('png', 'jpg');
+    // verify_input_maybe_value(lhm_editor_img_format, type_array, 'input');
+    // verify_input_maybe_value(lhm_editor_img_format, type_array, 'change');
+
+    let lhm_editor_img_format_value = lhm_editor_img_format.value;
+    console.log(lhm_editor_img_format.value);
+    lhm_editor_img_format.addEventListener('input', function () {
+        lhm_editor_img_format_value = lhm_editor_img_format.value;
+        console.log(lhm_editor_img_format_value);
+    });
+
+    function verify_input_maybe_value(element, maybe_value = [], type = '') {
+        let element_value = element.value;
+
+        element.addEventListener(type, function () {
+            element_value = element.value;
+        });
+
+        if (maybe_value.includes(element_value)) {
+            console.log(element_value);
+            return element_value;
+        } else {
+            return false;
+        }
+
+    }
+
+});
