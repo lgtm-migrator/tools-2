2e6ad1d5161035380e2a2be065ae83e7c2eca025
zhangchenglin <469946668@qq.com>
2020-03-12 13:06:56 +0800

    调整 改用本地静态文件

--- qrcode/q.php
+++ qrcode/q.php
@@ -16,7 +16,8 @@ if ($_POST) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css">
+<!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css">
     <title>灵活码结果</title>
 </head>
 <body class="min-vh-100"
@@ -44,8 +45,11 @@ if ($_POST) {
 </div>
 
 <div class="d-none">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+<!--    <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>-->
+<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>-->
+    <script src="/static/js/jquery.min.js"></script>
+    <script src="/static/js/popper.min.js"></script>
+    <script src="/static/js/bootstrap.min.js"></script>
 
     <script>
         let query = 'LHM-1582813189';
