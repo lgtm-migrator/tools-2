76899dd7f7da15ad160fc01c45e43e3136610632
zhangchenglin <469946668@qq.com>
2020-03-31 17:28:27 +0800

    整理 session

--- sql_back/jt_session.sql
+++ sql_back/jt_session.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 28/03/2020 19:50:36
+ Date: 31/03/2020 00:00:00
 */
 
 SET NAMES utf8mb4;
@@ -23,10 +23,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `jt_session`;
 CREATE TABLE `jt_session`  (
   `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '自增编号',
-  `session_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'ID',
-  `session_id_status` int(0) NOT NULL COMMENT 'ID的状态',
-  `session_info` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `session_expires` int(0) NOT NULL COMMENT '有效期',
+  `session_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'ID',
+  `session_id_status` int(0) NULL DEFAULT NULL COMMENT 'ID的状态',
+  `session_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `session_value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `session_data` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `session_lifetime` int(0) NULL DEFAULT NULL COMMENT 'lifetime',
+  `session_expires` int(0) NULL DEFAULT NULL COMMENT '有效期',
   `session_domain` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '作用域名',
   `session_httpOnly` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '仅限服务器',
   `session_maxAge` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
