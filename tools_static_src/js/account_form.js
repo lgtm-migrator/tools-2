c9ea0c9bbda3aa66f1edee6c6be9ae61751486e5
zhangchenglin <469946668@qq.com>
2020-04-13 16:35:49 +0800

    删除 更多登录方式按钮

--- tools_static_src/js/account_form.js
+++ tools_static_src/js/account_form.js
@@ -13,35 +13,6 @@
   }
 })();
 
-// 更多登录方式
-(function () {
-  let collapse_oauth_login = document.querySelector("#collapse_oauth_login");
-  let oauth_login_list = document.querySelector("#oauth_login_list");
-  if (collapse_oauth_login) {
-    collapse_oauth_login.addEventListener("click", collapse_oauth_login_toggle);
-    collapse_oauth_login.addEventListener("click", collapse_oauth_login_toggle_icon);
-  }
-
-  function collapse_oauth_login_toggle() {
-    if (oauth_login_list) {
-      $("#oauth_login_list").collapse("toggle");
-    }
-  }
-
-  function collapse_oauth_login_toggle_icon() {
-    let i = collapse_oauth_login.querySelector("i");
-    let $oauth_login_list = $("#oauth_login_list");
-    $oauth_login_list.on('hidden.bs.collapse', function () {
-      i.classList.remove("fa-chevron-down");
-      i.classList.add("fa-chevron-up");
-    });
-    $oauth_login_list.on('shown.bs.collapse', function () {
-      i.classList.remove("fa-chevron-up");
-      i.classList.add("fa-chevron-down");
-    });
-  }
-})();
-
 // 登录切换
 (function () {
   let sign_tab = document.querySelector('#sign_tab');
