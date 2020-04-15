053f06fb01bfadcacffa3a9d5cbb525c77653f6b
zhangchenglin <469946668@qq.com>
2020-04-16 01:00:37 +0800

    调整 手机号登录样式

--- wwwroot/member/account_form/sign_in_phone.php
+++ wwwroot/member/account_form/sign_in_phone.php
@@ -5,17 +5,29 @@
     </div>
     <div class="col-12 col-sm-10 input-group">
       <div class="input-group-prepend">
-        <div class="input-group-text">
-          <div class="btn-group btn-group-sm btn-group-vertical">
-            <button class="btn btn-outline-secondary" type="button">+86</button>
+        <div class="input-group-text dropdown-toggle" data-toggle="dropdown" id="phone_area">+86</div>
+        <div class="dropdown-menu min-w-rem-2 text-center">
+          <div class="btn-group-vertical btn-group-toggle" data-toggle="buttons">
+            <label class="min-w-rem-2 border-0 rounded-0 btn btn-outline-secondary" for="phone_area_111">+111<input
+                type="radio" id="phone_area_111" value="111"></label>
+            <label class="min-w-rem-2 border-0 rounded-0 btn btn-outline-secondary" for="phone_area_222">+222<input
+                type="radio" id="phone_area_222" value="222"></label>
+            <label class="min-w-rem-2 border-0 rounded-0 btn btn-outline-secondary" for="phone_area_333">+333<input
+                type="radio" id="phone_area_333" value="333"></label>
+            <label class="min-w-rem-2 border-0 rounded-0 btn btn-outline-secondary" for="phone_area_444">+444<input
+                type="radio" id="phone_area_444" value="444"></label>
+            <label class="min-w-rem-2 border-0 rounded-0 btn btn-outline-secondary" for="phone_area_555">+555<input
+                type="radio" id="phone_area_555" value="555"></label>
+            <label class="min-w-rem-2 border-0 rounded-0 btn btn-outline-secondary" for="phone_area_666">+666<input
+                type="radio" id="phone_area_666" value="666"></label>
           </div>
         </div>
       </div>
-      <input class="form-control" type="text" id="signIn_phone" placeholder="手机号码" minlength="5"
-             maxlength="20" autocomplete="off" required>
+      <input class="form-control" type="text" id="signIn_phone" placeholder="手机号码" minlength="11" maxlength="11"
+             autocomplete="off" required>
     </div>
   </div>
   <div class="d-flex justify-content-center">
-    <button class="btn btn-secondary" type="button" id="sign_in_phone_submit">找回密码</button>
+    <button class="btn btn-secondary" type="button" id="sign_in_phone_submit">登录</button>
   </div>
 </div>
--- wwwroot/member/account_form/sign_tab_content.php
+++ wwwroot/member/account_form/sign_tab_content.php
@@ -4,7 +4,7 @@
   <div class="tab-pane fade" id="sign_in">
     <?php require_once dirname(__FILE__) . '/sign_in.php'; ?>
   </div>
-  <div class="tab-pane fade active show" id="sign_in_phone">
+  <div class="tab-pane fade" id="sign_in_phone">
     <?php require_once dirname(__FILE__) . '/sign_in_phone.php'; ?>
   </div>
   <div class="tab-pane fade" id="sign_up">
