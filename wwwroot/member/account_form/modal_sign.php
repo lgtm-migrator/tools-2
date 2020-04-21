76a942f173775ba262aa3529583a15ff4d27d76a
zhangchenglin <469946668@qq.com>
2020-04-21 13:39:00 +0800

    添加 几个文件和svg图像备用

---
+++ wwwroot/member/account_form/modal_sign.php
@@ -0,0 +1,10 @@
+<div class="modal" id="sign" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content bg-light">
+      <div class="modal-body">
+        <?php require_once dirname(__FILE__) . '/sign_tab_content.php'; ?>
+        <?php include_once dirname(__FILE__) . '/sign_footer.php'; ?>
+      </div>
+    </div>
+  </div>
+</div>
