8d989a05ffb0183959972dc001a7f71f6937477d
zhangchenglin <469946668@qq.com>
2020-04-21 14:49:55 +0800

    调整 样式

--- wwwroot/member/account_form/sign_tab.php
+++ wwwroot/member/account_form/sign_tab.php
@@ -1,10 +1,10 @@
 <div class="w-75 w-sm-50 mb-2 btn-group btn-group-sm" id="sign_tab">
   <button class="btn btn-outline-secondary d-flex justify-content-center align-items-center sign_tab" type="button" data-target="#tab-sign_in">
-      <i class="fas fa-lg fa-sign-in-alt text-darkorange"></i>
-      <span class="ml-1">登录</span>
-    </button>
+    <i class="fas fa-lg fa-sign-in-alt text-darkorange"></i>
+    <span class="ml-1">登录</span>
+  </button>
   <button class="btn btn-outline-secondary d-flex justify-content-center align-items-center sign_tab" type="button" data-target="#tab-sign_up">
-      <i class="fas fa-lg fa-user-plus text-yellowgreen"></i>
-      <span class="ml-1">注册</span>
-    </button>
+    <i class="fas fa-lg fa-user-plus text-yellowgreen"></i>
+    <span class="ml-1">注册</span>
+  </button>
 </div>
