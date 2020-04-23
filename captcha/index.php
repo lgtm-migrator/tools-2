b122f03feec1c8ffedc7bbe4061f3c766d6c80c4
zhangchenglin <469946668@qq.com>
2020-04-23 10:37:26 +0800

    修复 微调验证码雏形

--- captcha/index.php
+++ captcha/index.php
@@ -1,14 +1,15 @@
 <?php
 require_once dirname(__FILE__) . "/captcha.php";
 
-//global $captcha;
+$captcha_dir = dirname(__DIR__) . '/wwwroot/captcha/';
+if (!file_exists($captcha_dir)) mk_dir($captcha_dir);
 
 $captcha
 //    ->setBackgroundImages(array())
   ->setBackgroundColor(255, 255, 255)
   ->setIgnoreAllEffects(true)
   ->build(100, 24)
-  ->save(dirname(__DIR__) . '/wwwroot/captcha/captcha.jpg', 50);
+  ->save($captcha_dir . '/captcha.jpg', 50);
 
 //$captcha->get(90);
 //$captcha->output(90);
