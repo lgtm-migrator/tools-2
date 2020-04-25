923a363f96101a3312b1d5b76f60f3cdd29cb48f
zhangchenglin <469946668@qq.com>
2020-04-25 12:01:45 +0800

    调整 第三方社交账号样式

--- wwwroot/member/account_form/oauth_sign_in.php
+++ wwwroot/member/account_form/oauth_sign_in.php
@@ -1,15 +1,14 @@
-<button class="btn btn-sm btn-outline-secondary" type="button" data-toggle="collapse" data-target="#oauth_sign_in">第三方社交账号登录</button>
-<div class="mt-1 px-1 collapse" id="oauth_sign_in">
-  <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-alipay" data-title="支付宝登录"></button>
-  <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-weixin" data-title="微信登录"></button>
-  <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-qq" data-title="QQ登录"></button>
-  <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-github" data-title="GitHub登录"></button>
-  <button type="button" class="border-0 btn btn-sm btn-outline-secondary dropdown-toggle" data-toggle="dropdown">更多</button>
-  <div class="px-1 border-0 dropdown-menu dropdown-menu-right">
+<a class="mt-2 d-flex flex-column align-items-center text-orangered text-decoration-none" href="javascript:" title="第三方社交账号登录" data-toggle="collapse" data-target="#oauth_sign_in">
+  <div class="my-1 px-1 collapse" id="oauth_sign_in">
+    <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-alipay" data-title="支付宝登录"></button>
+    <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-weixin" data-title="微信登录"></button>
+    <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-qq" data-title="QQ登录"></button>
+    <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-github" data-title="GitHub登录"></button>
     <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-google" data-title="谷歌登录"></button>
     <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-weibo" data-title="微博登录"></button>
     <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-facebook" data-title="脸书登录"></button>
     <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-twitter" data-title="推特登录"></button>
     <button type="button" class="p-2 border-0 btn btn-outline-secondary fab fa-lg fa-linkedin" data-title="领英登录"></button>
   </div>
-</div>
+  <span class="px-3 py-1 d-flex align-self-end align-items-center border rounded small text-orangered"><i class="mr-1 fas fa-lg fa-sign-in-alt"></i>第三方社交账号登录</span>
+</a>
--- wwwroot/member/account_form/sign_footer.php
+++ wwwroot/member/account_form/sign_footer.php
@@ -2,6 +2,4 @@
 <div class="mt-3 d-flex flex-column align-items-center">
   <?php include_once dirname(__FILE__) . '/sign_tab.php' ?>
 </div>
-<div class="mt-1 d-flex flex-column align-items-end align-items-sm-center">
-  <?php include_once dirname(__FILE__) . '/oauth_sign_in.php' ?>
-</div>
+<?php include_once dirname(__FILE__) . '/oauth_sign_in.php' ?>
