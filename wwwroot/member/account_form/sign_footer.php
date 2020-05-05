2f0abfe9ace4a1e8b88ac004025befe60f504726
zhangchenglin <469946668@qq.com>
2020-05-05 12:27:10 +0800

    调整 结构

--- wwwroot/member/account_form/sign_footer.php
+++ wwwroot/member/account_form/sign_footer.php
@@ -1,5 +1,6 @@
 <div class="dropdown-divider border-top-dotted"></div>
 <div class="d-flex flex-column align-items-center">
+  <button class="mb-2 d-flex align-self-end btn btn-sm btn-outline-danger" type="button" data-dismiss="modal">&times;</button>
   <?php include_once dirname(__FILE__) . '/sign_tab.php' ?>
 </div>
 <?php include_once dirname(__FILE__) . '/oauth_sign_in.php' ?>
--- wwwroot/member/account_form/sign_tab.php
+++ wwwroot/member/account_form/sign_tab.php
@@ -1,8 +1,3 @@
-<a class="mb-2 px-2 border rounded text-decoration-none text-muted" href="javascript:" data-dismiss="modal">
-  <span class="fas">&times;</span>
-  <span class="small">关闭</span>
-  <span class="fas">&times;</span>
-</a>
 <div class="w-75 w-sm-50 mb-2 btn-group btn-group-sm" id="sign_tab">
   <button class="btn btn-outline-secondary d-flex justify-content-center align-items-center sign_tab" type="button" data-target="#tab-sign_in">
     <i class="fas fa-lg fa-sign-in-alt text-darkorange"></i>
