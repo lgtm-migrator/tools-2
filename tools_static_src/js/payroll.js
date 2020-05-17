ecaae70432a9a186fe143bda0fcffdebb984a32d
zhangchenglin <469946668@qq.com>
2020-05-18 02:08:20 +0800

    调整 工资单雏形

---
+++ tools_static_src/js/payroll.js
@@ -0,0 +1,58 @@
+// 悬浮弹出框
+$().ready(function () {
+  let jt_payroll_pension_icon = document.querySelector('#jt_payroll_pension_icon');
+  let jt_payroll_actual_salary_icon = document.querySelector('#jt_payroll_actual_salary_icon');
+  let jt_payroll_housing_fund_icon = document.querySelector('#jt_payroll_housing_fund_icon');
+  if (jt_payroll_pension_icon) {
+    $('#jt_payroll_pension_icon').popover({
+      trigger: 'hover',
+      boundary: 'viewport',
+      placement: 'auto',
+      html: true,
+      content: popover_content_jt_payroll_pension_icon,
+    });
+  }
+  if (jt_payroll_actual_salary_icon) {
+    $('#jt_payroll_actual_salary_icon').popover({
+      trigger: 'hover',
+      boundary: 'viewport',
+      placement: 'auto',
+      html: true,
+      content: popover_content_jt_payroll_actual_salary_icon,
+    });
+  }
+  if (jt_payroll_housing_fund_icon) {
+    $('#jt_payroll_housing_fund_icon').popover({
+      trigger: 'hover',
+      boundary: 'viewport',
+      placement: 'auto',
+      html: true,
+      content: popover_content_jt_payroll_housing_fund_icon,
+    });
+  }
+});
+
+function popover_content_jt_payroll_pension_icon() {
+  let span = document.createElement("span");
+
+  span.className = 'small';
+  span.innerHTML = '1.个人缴费根据职工本人上一年度月平均工资(最低数为上年全市职工工资的60%;最高数为上年全市职工工资的300%)的8%缴纳。' + '<br>' +
+    '2.单位缴费根据职工本人上一年度月平均工资的22%缴纳。2006年1月1日起，人社部将个人养老账户的规模统一由本人缴费工资的11%调整为8%。此前的政策是个人缴费全部和单位缴费的3%计入个人养老账户，单位缴纳的19%划转为社会统筹，而新政策将单位缴费的3%也划入社会统筹用来解决养老空账问题。';
+  return span;
+}
+
+function popover_content_jt_payroll_actual_salary_icon() {
+  let span = document.createElement("span");
+
+  span.className = 'small';
+  span.innerHTML = '输入完税前工资和各种保险和代扣费用后，点击下方计算按钮即可算出实际工资金额。';
+  return span;
+}
+
+function popover_content_jt_payroll_housing_fund_icon() {
+  let span = document.createElement("span");
+
+  span.className = 'small';
+  span.innerHTML = '公积金缴费比例：根据企业的实际情况，选择住房公积金缴费比例。但原则上最高缴费额不得超过北京市职工平均工资的10%。2009年下半年起，北京市统一规定所有用人单位按工资的12%办理缴纳住房公积金。单位和个人都是工资的12%。';
+  return span;
+}
--- wwwroot/member/payroll/index.php
+++ wwwroot/member/payroll/index.php
@@ -7,8 +7,266 @@ if (!defined('JZEG_NET')) die();
   <div class="py-2 container" id="jt_payroll">
     <div class="mb-2 font-weight-bolder">工资单</div>
     <form class="px-3 py-2 rounded border" action="/member/payroll/payroll.php" method="post" enctype="multipart/form-data">
-      <div class="form-group text-center">
-        <button type="submit" class="btn btn-outline-success" id="jt_payroll_submit">保存</button>
+      <div class="form-row form-group">
+        <div class="col-12 col-md-6 mb-2 mb-md-0">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_name">日期</label>
+            </div>
+            <input type="date" class="form-control" id="jt_payroll_date">
+          </div>
+          <div class="small text-muted">
+            <span class="small">几月份的工资</span>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 mb-2 mb-md-0">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_name">日期</label>
+            </div>
+            <input type="date" class="form-control" id="jt_payroll_date">
+          </div>
+          <div class="small text-muted">
+            <span class="small">工资到账时间</span>
+          </div>
+        </div>
+      </div>
+      <div class="form-row form-group">
+        <div class="col-12 col-md-4 mb-2 mb-md-0">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_name">姓名</label>
+            </div>
+            <input type="text" class="form-control" id="jt_payroll_name" placeholder="姓名">
+          </div>
+        </div>
+        <div class="col-12 col-md-4 mb-2 mb-md-0">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_job_number">工号</label>
+            </div>
+            <input type="text" class="form-control" id="jt_payroll_job_number" placeholder="工号">
+          </div>
+          <div class="small text-muted">
+            <span class="small">工号</span>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_personnel_number">人员编号</label>
+            </div>
+            <input type="text" class="form-control" id="jt_payroll_personnel_number" placeholder="人员编号">
+          </div>
+          <div class="small text-muted">
+            <span class="small">单位人员编号</span>
+          </div>
+        </div>
+      </div>
+      <div class="form-row form-group">
+        <div class="col-12 col-md-6 mb-2 mb-md-0">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_payable">应发工资</label>
+            </div>
+            <input type="text" class="form-control rounded-right" id="jt_payroll_payable" placeholder="应发工资">
+          </div>
+          <div class="small text-muted">
+            <span class="small">未扣五险一金和各种代扣费用之前</span>
+          </div>
+        </div>
+        <div class="col-12 col-md-6">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_actual_salary">实发工资</label>
+            </div>
+            <input type="text" class="form-control" id="jt_payroll_actual_salary" placeholder="自动计算出结果" readonly>
+            <div class="input-group-append">
+              <span class="input-group-text">
+                <i class="fas fa-question-circle" id="jt_payroll_actual_salary_icon"></i>
+              </span>
+            </div>
+          </div>
+          <div class="small text-muted">
+            <span class="small">自动计算实发到卡金额</span>
+          </div>
+        </div>
+      </div>
+      <div class="form-row form-group">
+        <div class="col-12 col-md-6 mb-2">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_medical_insurance_number">医疗保险比例</label>
+            </div>
+            <input type="number" class="form-control text-center" min="1" max="10" step="1" value="2" id="jt_payroll_medical_insurance_number" readonly>
+            <div class="input-group-append">
+              <span class="input-group-text">%  +</span>
+            </div>
+            <input type="number" class="form-control text-center" min="1" max="10" step="1" value="3" id="jt_payroll_medical_insurance_number_push">
+            <div class="input-group-append">
+              <span class="input-group-text">元</span>
+            </div>
+          </div>
+          <div class="small text-muted">
+            <span>医疗保险缴费比例：单位10%，个人2%+3元</span>
+          </div>
+          <div>
+            <label class="sr-only" for="jt_payroll_medical_insurance_range">医疗保险比例</label>
+            <input type="range" class="custom-range" min="1" max="10" step="1" value="2" id="jt_payroll_medical_insurance_range">
+          </div>
+        </div>
+        <div class="col-12 col-md-6 mb-2">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_medical_insurance_number">失业保险比例</label>
+            </div>
+            <input type="number" class="form-control text-right" min="0" max="1.5" step="0.5" value="0.5" id="jt_payroll_medical_insurance_number" readonly>
+            <div class="input-group-append">
+              <span class="input-group-text">%</span>
+            </div>
+          </div>
+          <div class="small text-muted">
+            <span>失业保险缴费比例：单位1.5%，个人0.5%</span>
+          </div>
+          <div>
+            <label class="sr-only" for="jt_payroll_medical_insurance_range">失业保险比例</label>
+            <input type="range" class="custom-range" min="0" max="1.5" step="0.5" value="0.5" id="jt_payroll_medical_insurance_range">
+          </div>
+        </div>
+      </div>
+      <div class="form-row form-group">
+        <div class="col-12 col-md-6 mb-2">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_medical_insurance_number">工伤保险比例</label>
+            </div>
+            <input type="number" class="form-control text-right" min="0" max="2" step="0.1" value="0" id="jt_payroll_medical_insurance_number" readonly>
+            <div class="input-group-append">
+              <span class="input-group-text">%</span>
+            </div>
+          </div>
+          <div class="small text-muted">
+            <span>工伤保险缴费比例：单位0.5%~2%，个人0</span>
+          </div>
+          <div>
+            <label class="sr-only" for="jt_payroll_medical_insurance_range">工伤保险比例</label>
+            <input type="range" class="custom-range" min="0" max="2" step="0.1" value="0" id="jt_payroll_medical_insurance_range">
+          </div>
+        </div>
+        <div class="col-12 col-md-6 mb-2">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_medical_insurance_number">生育保险比例</label>
+            </div>
+            <input type="number" class="form-control text-right" min="1" max="10" step="1" value="8" id="jt_payroll_medical_insurance_number" readonly>
+            <div class="input-group-append">
+              <span class="input-group-text">%</span>
+            </div>
+          </div>
+          <div class="small text-muted">
+            <span>生育保险缴费比例：单位0.8%，个人不交钱</span>
+          </div>
+          <div>
+            <label class="sr-only" for="jt_payroll_medical_insurance_range">生育保险比例</label>
+            <input type="range" class="custom-range" min="1" max="10" step="1" value="8" id="jt_payroll_medical_insurance_range">
+          </div>
+        </div>
+      </div>
+      <div class="form-row form-group">
+        <div class="col-12 col-md-6 mb-2">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_pension_number">养老保险金额</label>
+            </div>
+            <input type="number" class="form-control text-right" placeholder="养老保险金额" id="jt_payroll_pension_number">
+            <div class="input-group-append">
+              <span class="input-group-text">元</span>
+              <span class="input-group-text">
+                <i class="fas fa-question-circle" id="jt_payroll_pension_icon"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 mb-2">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_housing_fund">住房公积金金额</label>
+            </div>
+            <input type="number" class="form-control text-right" placeholder="住房公积金金额" id="jt_payroll_housing_fund">
+            <div class="input-group-append">
+              <span class="input-group-text">元</span>
+              <span class="input-group-text">
+                <i class="fas fa-question-circle" id="jt_payroll_housing_fund_icon"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="form-row form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <label class="input-group-text" for="jt_payroll_pension_adjustment_difference">养老保险调差额</label>
+          </div>
+          <input type="number" class="form-control text-right" placeholder="养老保险调差额"
+                 id="jt_payroll_pension_adjustment_difference">
+          <div class="input-group-append">
+            <span class="input-group-text">元</span>
+            <span class="input-group-text">
+                <i class="fas fa-question-circle" id="jt_payroll_pension_adjustment_difference_icon"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="form-row form-group">
+        <div class="col-12 col-md-4 mb-2">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_telephone_fee_number">电话费</label>
+            </div>
+            <input type="number" class="form-control text-right" placeholder="电话费" id="jt_payroll_telephone_fee_number">
+            <div class="input-group-append">
+              <span class="input-group-text">元</span>
+            </div>
+          </div>
+          <div class="small text-muted">
+            <span class="small">代扣电话费金额</span>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-4 mb-2">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_accommodation_fee_number">标宿费</label>
+            </div>
+            <input type="number" class="form-control text-right" placeholder="标宿费" id="jt_payroll_accommodation_fee_number">
+            <div class="input-group-append">
+              <span class="input-group-text">元</span>
+            </div>
+          </div>
+          <div class="small text-muted">
+            <span class="small">代扣标宿费金额</span>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-4 mb-2">
+          <div class="mb-1 input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="jt_payroll_utilities_number">水电费</label>
+            </div>
+            <input type="number" class="form-control text-right" placeholder="水电费" id="jt_payroll_utilities_number">
+            <div class="input-group-append">
+              <span class="input-group-text">元</span>
+            </div>
+          </div>
+          <div class="small text-muted">
+            <span class="small">代扣水电费金额</span>
+          </div>
+        </div>
+      </div>
+      <div class="form-group text-center btn-group-sm">
+        <button type="reset" class="btn btn-outline-secondary" id="jt_payroll_reset">重置</button>
+        <button type="button" class="btn btn-outline-success" id="jt_payroll_compute">计算工资</button>
+        <button type="submit" class="btn btn-outline-primary" id="jt_payroll_submit">上传保存</button>
       </div>
     </form>
   </div>
