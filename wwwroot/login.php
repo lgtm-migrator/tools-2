8f8bf775dcebc857629c9c19097f6f27ceb64e11
zhangchenglin <469946668@qq.com>
2020-06-08 01:12:08 +0800

    调整  精简代码

--- wwwroot/login.php
+++ wwwroot/login.php
@@ -4,25 +4,12 @@ require_once dirname(__FILE__) . '/common/blank/header.php';
 if (!defined('SITE_HEAD')) die();
 ?>
 
-<div class="mt-5 container">
-  <div class="row row-cols-1 row-cols-lg-2">
-    <div class="p-5"></div>
+<div class="container">
+  <div class="d-flex align-items-center justify-content-center flex-column-reverse flex-md-row">
+    <?php include_once dirname(__FILE__) . '/common/ad/member_sign_ad.php'; ?>
     <div class="p-5 shadow rounded bg-white">
-      <div class="tab-content">
-        <div class="tab-pane fade active show" id="tab-sign_in">
-          <?php include_once dirname(__FILE__) . '/member/account_form/sign_in.php'; ?>
-        </div>
-        <div class="tab-pane fade" id="tab-sign_in_phone">
-          <?php include_once dirname(__FILE__) . '/member/account_form/sign_in_phone.php'; ?>
-        </div>
-        <div class="tab-pane fade" id="tab-sign_up">
-          <?php include_once dirname(__FILE__) . '/member/account_form/sign_up.php'; ?>
-        </div>
-        <div class="tab-pane fade" id="tab-password_reset">
-          <?php include_once dirname(__FILE__) . '/member/account_form/password_reset.php'; ?>
-        </div>
-      </div>
-      <?php include_once dirname(__FILE__) . '/member/account_form/sign_footer.php'; ?>
+      <?php include_once dirname(__FILE__) . '/member/account_form/sign_in.php'; ?>
+      <?php include_once dirname(__FILE__) . '/member/account_form/oauth_sign_in.php'; ?>
     </div>
   </div>
 </div>
@@ -34,4 +21,3 @@ if (!defined('SITE_HEAD')) die();
 <?php
 require_once dirname(__FILE__) . '/common/blank/footer.php';
 ?>
-
