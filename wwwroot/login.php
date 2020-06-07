16b50ee9132dca6fdbe81502b82089514e58a4c8
zhangchenglin <469946668@qq.com>
2020-06-07 17:15:49 +0800

    调整  结构

--- wwwroot/login.php
+++ wwwroot/login.php
@@ -4,10 +4,10 @@ require_once dirname(__FILE__) . '/common/blank/header.php';
 if (!defined('SITE_HEAD')) die();
 ?>
 
-<div class="mt-3 container">
-  <div class="row">
-    <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6"></div>
-    <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+<div class="mt-5 container">
+  <div class="row row-cols-1 row-cols-lg-2">
+    <div class="p-5"></div>
+    <div class="p-5 bg-white">
       <div class="tab-content">
         <div class="tab-pane fade active show" id="tab-sign_in">
           <?php include_once dirname(__FILE__) . '/member/account_form/sign_in.php'; ?>
