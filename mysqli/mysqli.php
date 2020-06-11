23d9981b018f17bca431687f5b423ed0c4bbe43a
zhangchenglin <469946668@qq.com>
2020-06-11 16:54:35 +0800

    调整  改用env文件记录敏感信息

--- mysqli/mysqli.php
+++ mysqli/mysqli.php
@@ -1,14 +1,13 @@
 <?php
-require_once dirname(__FILE__) . "/config.php";
-require_once dirname(__DIR__) . "/vendor/autoload.php";
+require_once dirname(__DIR__) . "/config/env.php";
 
 
 $db = new MysqliDb();
 $db_addConnection_params = array(
-    'host' => $db_host,
-    'username' => $db_user,
-    'password' => $db_pwd,
-    'db' => $db_database,
+    'host' => $_ENV['DB_HOST'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'db' => $_ENV['DB_DATABASE_NAME'],
 //        'port' => 3306,
 //        'prefix' => '',
 //        'charset' => 'utf8'
@@ -20,9 +19,9 @@ $db->addConnection("recaptcha", $db_addConnection_params);
 $db->addConnection("jt_session", $db_addConnection_params);
 
 $db->addConnection("number_stored", array(
-    'host' => $db_host,
-    'username' => $db_user,
-    'password' => $db_pwd,
+  'host' => $_ENV['DB_HOST'],
+  'username' => $_ENV['DB_USERNAME'],
+  'password' => $_ENV['DB_PASSWORD'],
     'db' => "information_schema",
 //        'port' => 3306,
 //        'prefix' => '',
