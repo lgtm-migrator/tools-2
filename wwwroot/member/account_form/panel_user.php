ee7cf4e0afe843e663c7573d0c9cb33dedfe23fd
zhangchenglin <469946668@qq.com>
2020-06-23 19:33:38 +0800

    调整 增加淡入淡出

--- wwwroot/member/account_form/panel_user.php
+++ wwwroot/member/account_form/panel_user.php
@@ -1,4 +1,4 @@
-<div class="modal" id="sign" tabindex="-1">
+<div class="modal fade" id="sign" tabindex="-1">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-body">
