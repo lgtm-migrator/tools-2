e953b19e0d93dd2119cc2c752126e872772d91ca
zhangchenglin <469946668@qq.com>
2020-07-08 14:34:20 +0800

    调整 增加查询条件

--- class/MySQL/Operate/DbGet.php
+++ class/MySQL/Operate/DbGet.php
@@ -5,6 +5,7 @@ namespace JZEG_NET\MySQL\Operate;
 
 require_once dirname(__DIR__) . "/DbConnection/DataBaseConnection.php";
 
+use \Exception;
 use JZEG_NET\MySQL\DbConnection\DataBaseConnection;
 
 
@@ -21,7 +22,9 @@ class DbGet extends DataBaseConnection
   private $having;
   private $orHaving;
 
-  public function __construct(array $conn_params, string $query_key = null, string $table_name = null, $where = null, $having = null, $orWhere = null, $orHaving = null, $result_columns = null)
+  private $queryResult;
+
+  public function __construct(array $conn_params, string $query_key = null, string $table_name = null, array $where = null, array $having = null, array $orWhere = null, array $orHaving = null, array $result_columns = null)
   {
     if (is_array($query_key)) {
       foreach ($query_key as $key => $val) {
@@ -40,5 +43,39 @@ class DbGet extends DataBaseConnection
 
     $this->db = parent::__construct($this->conn_params);
 
+  }
+
+  public function setWhere(array $where_data)
+  {
+    parent::Where("category", $where_data['category']);
+  }
+
+  public function setLikeWhere(array $LikeWhere_data)
+  {
+    parent::where("text", "%" . $LikeWhere_data['key'] . "%", 'LIKE');
+  }
+
+  public function setLikeHaving(array $LikeHaving_data)
+  {
+    parent::having("text", "%" . $LikeHaving_data['key'] . "%", 'LIKE');
+  }
+
+  public function setQueryResult(array $query_data)
+  {
+    self::setWhere($query_data['conditions']['where']);
+
+//    self::setLikeHaving($query_data['likeHaving']);
+    self::setLikeWhere($query_data['conditions']['likeWhere']);
+
+    try {
+      $this->queryResult = parent::get($query_data['table_name'], null, $query_data['result_columns']);
+    } catch (Exception $e) {
+      die($e->getMessage());
+    }
+  }
+
+  public function getQueryResult()
+  {
+    return $this->queryResult;
   }
 }
