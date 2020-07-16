efef6c4f41f5521437e0e4bad4c192daf6c546dd
zhangchenglin <469946668@qq.com>
2020-07-17 07:29:57 +0800

    调整 改变入口文件

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -7,6 +7,6 @@ if (!defined('JZEG-NET')) die();
   <div class="container text-center small text-white-50" id="footer_x"></div>
 </div>
 </div><!--id=body-->
-<?php require_once dirname(__FILE__) . '/member/account_form/panel_user.php'; ?>
+<?php require_once dirname(__FILE__) . '/member/account_form/modal_sign.php'; ?>
 </body>
 </html>
