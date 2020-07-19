c78e5d9f6e87fe9ce0180e8b72e9bac250989508
zhangchenglin <469946668@qq.com>
2020-07-20 04:18:48 +0800

    调整 添加图片链接和视频链接的处理

--- tools_static_src/js/yjt/20200628_3/index.js
+++ tools_static_src/js/yjt/20200628_3/index.js
@@ -40,21 +40,31 @@ function set_category_text(category) {
 function question_bank(question_bank) {
   check_questions(eval(question_bank));
   let question_bank_code = eval(question_bank).map(function (e, t) {
+    e.forEach(function (eTriggerEL, index) {
+      if (true === RegExp_rules['video'].test(eTriggerEL)) {
+        e[index] = "" +
+          "<video class='embed-responsive' controls preload='none'>" +
+          "<source src='" + e[index] + "' type='video/mp4'>" +
+          "</video>";
+      } else if (true === RegExp_rules['image'].test(eTriggerEL)) {
+        e[index] = "<img class='my-1 img-fluid img-thumbnail' alt='img' src=" + e[index] + ">";
+      }
+    });
     return t % 2 === 0
       ? '<div class="px-2 py-3">' +
       '<p>' + e[0] + "</p>" +
       "<ul style='list-style-type: upper-latin;'>" +
-      "<li" + (1 === e[5] ? ' class="my-1 text-success font-weight-bold"' : "") + ">" + e[1] + "</li>" +
-      "<li" + (2 === e[5] ? ' class="my-1 text-success font-weight-bold"' : "") + ">" + e[2] + "</li>" +
-      "<li" + (3 === e[5] ? ' class="my-1 text-success font-weight-bold"' : "") + "" + (e[3] === "" ? ' class="d-none"' : "") + ">" + e[3] + "</li>" +
-      "<li" + (4 === e[5] ? ' class="my-1 text-success font-weight-bold"' : "") + "" + (e[4] === "" ? ' class="d-none"' : "") + ">" + e[4] + "</li>" +
+      "<li" + (1 === e[5] ? ' class="px-2 py-1 text-success font-weight-bold rounded border border-success" title="' + e[6] + '"' : "") + ">" + e[1] + "</li>" +
+      "<li" + (2 === e[5] ? ' class="px-2 py-1 text-success font-weight-bold rounded border border-success" title="' + e[6] + '"' : "") + ">" + e[2] + "</li>" +
+      "<li" + (3 === e[5] ? ' class="px-2 py-1 text-success font-weight-bold rounded border border-success" title="' + e[6] + '"' : "") + "" + (e[3] === "" ? ' class="d-none"' : "") + ">" + e[3] + "</li>" +
+      "<li" + (4 === e[5] ? ' class="px-2 py-1 text-success font-weight-bold rounded border border-success" title="' + e[6] + '"' : "") + "" + (e[4] === "" ? ' class="d-none"' : "") + ">" + e[4] + "</li>" +
       "</ul>"
       : "<p>" + e[0] + "</p>" +
       "<ol style='list-style-type: upper-latin;'>" +
-      "<li" + (1 === e[5] ? ' class="my-1 text-success font-weight-bold"' : "") + ">" + e[1] + "</li>" +
-      "<li" + (2 === e[5] ? ' class="my-1 text-success font-weight-bold"' : "") + ">" + e[2] + "</li>" +
-      "<li" + (3 === e[5] ? ' class="my-1 text-success font-weight-bold"' : "") + "" + (e[3] === "" ? ' class="d-none"' : "") + ">" + e[3] + "</li>" +
-      "<li" + (4 === e[5] ? ' class="my-1 text-success font-weight-bold"' : "") + "" + (e[4] === "" ? ' class="d-none"' : "") + ">" + e[4] + "</li>" +
+      "<li" + (1 === e[5] ? ' class="px-2 py-1 text-success font-weight-bold rounded border border-success" title="' + e[6] + '"' : "") + ">" + e[1] + "</li>" +
+      "<li" + (2 === e[5] ? ' class="px-2 py-1 text-success font-weight-bold rounded border border-success" title="' + e[6] + '"' : "") + ">" + e[2] + "</li>" +
+      "<li" + (3 === e[5] ? ' class="px-2 py-1 text-success font-weight-bold rounded border border-success" title="' + e[6] + '"' : "") + "" + (e[3] === "" ? ' class="d-none"' : "") + ">" + e[3] + "</li>" +
+      "<li" + (4 === e[5] ? ' class="px-2 py-1 text-success font-weight-bold rounded border border-success" title="' + e[6] + '"' : "") + "" + (e[4] === "" ? ' class="d-none"' : "") + ">" + e[4] + "</li>" +
       "</ol>" +
       "</div>";
   }).join("");
