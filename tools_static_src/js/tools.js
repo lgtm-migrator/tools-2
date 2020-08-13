965d4663d77d921a5877100dccce6a8775146b41
zhangchenglin <469946668@qq.com>
2020-08-13 23:18:54 +0800

    调整 底部二维码结构

--- tools_static_src/js/tools.js
+++ tools_static_src/js/tools.js
@@ -901,38 +901,28 @@ $().ready(function () {
 $().ready(function () {
   let socialGroupQrcode = document.querySelector('#socialGroupQrcode');
   if (socialGroupQrcode) {
-    let socialGroupCollapse_dingTalk = document.querySelector('#socialGroupCollapse_dingTalk');
-    let socialGroupCollapse_qq = document.querySelector('#socialGroupCollapse_qq');
-    let socialGroupCollapse_wechat = document.querySelector('#socialGroupCollapse_wechat');
     let dingTalk_img = document.createElement("img");
     let qq_img = document.createElement("img");
     let wechat_img = document.createElement("img");
+    let socialGroup_dingTalk = document.querySelector('#socialGroup_dingTalk');
+    let socialGroup_qq = document.querySelector('#socialGroup_qq');
+    let socialGroup_wechat = document.querySelector('#socialGroup_wechat');
 
-    let dingTalk_url_parameters = {
-      'corpId': 'ding893bad2d4e569096fac3b26d3f309cdb',
-      'ab55e19': '00f6ed1',
-      'cbdbhh': 'qwertyuiop',
-      'origin': '0',
-    };
-    let qq_url_parameters = {
-      'k': 'Ud6pogcHJ1ioQR_8qtPTHkXr48QRmtd4',
-      'authKey': 'dSaugxZBNfcmo4T9bg+ltEY5VeRbqvtCoo99VxHHxLv4/wWLDuejiKc+nbTJCY0d',
-      'noverify': '0',
-    };
-    let wechat_url_parameters = {};
-    let dingtalk_url = create_url('https://h5.dingtalk.com/', 'circle/healthCheckin.html', dingTalk_url_parameters);
+    let dingTalk_url_parameters = '?corpId=ding893bad2d4e569096fac3b26d3f309cdb&ab55e19=00f6ed1&cbdbhh=qwertyuiop&origin=0';
+    let qq_url_parameters = '?k=Ud6pogcHJ1ioQR_8qtPTHkXr48QRmtd4&authKey=dSaugxZBNfcmo4T9bg+ltEY5VeRbqvtCoo99VxHHxLv4/wWLDuejiKc+nbTJCY0d&noverify=0';
+    let wechat_url_parameters = '';
+    let dingTalk_url = create_url('https://h5.dingtalk.com/', 'circle/healthCheckin.html', dingTalk_url_parameters);
     let qq_url = create_url('https://qm.qq.com/', 'cgi-bin/qm/qr', qq_url_parameters);
     let wechat_url = create_url('https://weixin.qq.com/', 'g/AQYAADnWmR9tHL01ktpbeg2T69yCdP6wRAh2sRBbpFYoMk6oY3igS9hAnhKIfBXa', wechat_url_parameters);
 
-    create_socialGroup_qrcode(dingtalk_url, dingTalk_img, socialGroupCollapse_dingTalk);
-    create_socialGroup_qrcode(qq_url, qq_img, socialGroupCollapse_qq);
-    create_socialGroup_qrcode(wechat_url, wechat_img, socialGroupCollapse_wechat);
-
+    create_socialGroup_qrcode(dingTalk_url, dingTalk_img, socialGroup_dingTalk);
+    create_socialGroup_qrcode(qq_url, qq_img, socialGroup_qq);
+    create_socialGroup_qrcode(wechat_url, wechat_img, socialGroup_wechat);
   }
 })
 
-function create_url(baseDomain = '', pathname = '', parameters = []) {
-  return baseDomain + pathname;
+function create_url(baseDomain = '', pathname = '', parameters = '') {
+  return baseDomain + pathname + parameters;
 }
 
 function create_socialGroup_qrcode(socialGroupUrl = '', socialGroupElement, parentElement) {
@@ -952,8 +942,7 @@ function create_socialGroup_qrcode(socialGroupUrl = '', socialGroupElement, pare
     if (err) throw err;
     socialGroupElement.className = 'mb-2 img-thumbnail';
     socialGroupElement.src = img_base64;
-    socialGroupElement.alt = '钉钉群二维码';
-    socialGroupElement.title = '钉钉群二维码';
-    parentElement.appendChild(socialGroupElement);
+    socialGroupElement.alt = '二维码';
+    parentElement ? parentElement.appendChild(socialGroupElement) : '';
   });
 }
--- wwwroot/common/socialGroup.php
+++ wwwroot/common/socialGroup.php
@@ -2,32 +2,32 @@
   <div class="d-flex justify-content-center small text-secondary" id="socialGroup">
     <ul class="list-group list-group-horizontal justify-content-sm-center">
       <li class="list-group-item border-0 bg-transparent text-reset">
-        <a class="d-flex align-items-center text-reset text-decoration-none" href="javascript:" data-title="钉钉联系方式" data-target="#socialGroupCollapse_dingTalk">
+        <a class="d-flex align-items-center text-reset text-decoration-none" href="javascript:" data-title="钉钉联系方式" data-target="#socialGroup_dingTalk">
           <span class="bg-size-25 bg-repeat-no bg-logo-dingtalk"></span>
           <span>钉钉</span>
         </a>
       </li>
       <li class="list-group-item border-0 bg-transparent text-reset">
-        <a class="d-flex align-items-center text-reset text-decoration-none" href="javascript:" data-title="QQ联系方式" data-target="#socialGroupCollapse_qq">
+        <a class="d-flex align-items-center text-reset text-decoration-none" href="javascript:" data-title="QQ联系方式" data-target="#socialGroup_qq">
           <span class="bg-size-25 bg-repeat-no bg-logo-qq"></span>
           <span>QQ</span>
         </a>
       </li>
       <li class="list-group-item border-0 bg-transparent text-reset">
-        <a class="d-flex align-items-center text-reset text-decoration-none" href="javascript:" data-title="微信联系方式" data-target="#socialGroupCollapse_wechat">
+        <a class="d-flex align-items-center text-reset text-decoration-none" href="javascript:" data-title="微信联系方式" data-target="#socialGroup_wechat">
           <span class="bg-size-25 bg-repeat-no bg-logo-wechat"></span>
           <span>微信</span>
         </a>
       </li>
     </ul>
     <div class="d-none" id="socialGroupQrcode">
-      <div class="d-flex flex-column align-items-center" id="socialGroupCollapse_dingTalk">
+      <div class="d-flex flex-column align-items-center" id="socialGroup_dingTalk">
         <div>群号码：30207213</div>
       </div>
-      <div class="d-flex flex-column align-items-center" id="socialGroupCollapse_qq">
+      <div class="d-flex flex-column align-items-center" id="socialGroup_qq">
         <div>群号码：480073883</div>
       </div>
-      <div class="d-flex flex-column align-items-center" id="socialGroupCollapse_wechat">
+      <div class="d-flex flex-column align-items-center" id="socialGroup_wechat">
         <div>微信号：JZEG-NET</div>
       </div>
     </div>
