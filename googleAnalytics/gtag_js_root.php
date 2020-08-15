42b0f09f62a7d63ad5776f17e9d4c49c4b849534
zhangchenglin <469946668@qq.com>
2020-08-16 00:52:12 +0800

    调整 gtag.js方式

---
+++ googleAnalytics/gtag_js_root.php
@@ -0,0 +1,15 @@
+<?php
+require_once dirname(__DIR__) . '/config/env.php';
+?>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $_ENV['GOOGLE_ANALYTICS_CONFIG_ROOT']; ?>"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+  gtag('config', "<?php echo $_ENV['GOOGLE_ANALYTICS_CONFIG_ROOT']; ?>");
+</script>
