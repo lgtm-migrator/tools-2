2ecc9e2387720b4411ce78dbb29f4f8583e54ac3
zhangchenglin <469946668@qq.com>
2020-08-16 01:45:45 +0800

    调整 布局

---
+++ wwwroot/www/index.php
@@ -0,0 +1,25 @@
+<?php
+define('title', '在线小工具');
+require_once dirname(__DIR__) . '/header.php';
+if (!defined('SITE_HEAD')) die();
+?>
+<div class="container">
+  <div class="d-flex justify-content-center align-items-center min-vh-90">
+    <div class="card bg-light">
+      <div class="card-header">
+        <span class="h5 mb-0">欢迎光临杰格网</span>
+      </div>
+      <div class="card-body">
+        <span>杰格网正在建设中，请先加入页面底部的钉钉群进行讨论，微信群和QQ群为辅助群讨论。</span>
+        <span class="mt-2 d-flex justify-content-end">2020.08.16</span>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="d-none">
+  <?php require_once dirname(__DIR__) . "/javascript.php"; ?>
+</div>
+
+<?php
+require_once dirname(__DIR__) . '/footer.php';
+?>
