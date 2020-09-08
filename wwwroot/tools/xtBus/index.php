fe9dafb4b172bbe609f15efc7978f9f464fa7396
zhangchenglin <469946668@qq.com>
2020-09-09 00:38:26 +0800

    调整 样式

--- wwwroot/tools/xtBus/index.php
+++ wwwroot/tools/xtBus/index.php
@@ -1,18 +1,23 @@
 <?php
-define('title', '公交线路信息查询');
+define('title', '邢台公交路线实时查询');
+define('descriptionContent', '提供实时查询邢台公交车路线信息、站牌信息、倒车转乘路线信息。');
+define('keywordsContent', '公交车实时查询,公交车实时路线,公交车查询,邢台公交,实时公交查询,实时线路查询,掌上公交');
 require_once dirname(dirname(__DIR__)) . "/header.php";
 ?>
 <link rel="stylesheet" href="/static/css/xtBus/index.min.css">
 
 <div class="container" id="jt_container">
   <div id="bus">
-    <div class="font-weight-bolder text-text-dodgerblue" id="busArea"></div>
+    <div class="py-3 font-weight-bolder text-text-dodgerblue" id="busArea"></div>
     <div class="card">
       <div class="card-header">
         <div class="nav justify-content-between justify-content-sm-around justify-content-md-evenly" id="busQuery">
           <div class="btn-group btn-group-sm dropdown">
-            <button class="btn btn-outline-primary active" type="button" role="tab" data-target="#lines" aria-controls="lines" aria-selected="true" aria-pressed="true"><i class="mr-1 fas fa-bus"></i>查线路</button>
-            <button class="btn btn-outline-primary dropdown-toggle dropdown-toggle-split" type="button" data-toggle="dropdown" aria-expanded="true">
+            <button class="btn btn-outline-primary active" type="button" role="tab" data-target="#lines"
+                    aria-controls="lines" aria-selected="true" aria-pressed="true"><i class="mr-1 fas fa-bus"></i>查线路
+            </button>
+            <button class="btn btn-outline-primary dropdown-toggle dropdown-toggle-split" type="button"
+                    data-toggle="dropdown" aria-expanded="true">
               <span class="sr-only">切换下拉菜单</span>
             </button>
             <div class="p-3 dropdown-menu dropdown-menu-right">
@@ -23,8 +28,11 @@ require_once dirname(dirname(__DIR__)) . "/header.php";
             </div>
           </div>
           <div class="btn-group btn-group-sm dropdown">
-            <button class="btn btn-outline-info" type="button" role="tab" data-target="#stations" aria-controls="stations" aria-selected="false" aria-pressed="false"><i class="mr-1 fas fa-sign"></i>查站牌</button>
-            <button class="btn btn-outline-info dropdown-toggle dropdown-toggle-split" type="button" data-toggle="dropdown" aria-expanded="false">
+            <button class="btn btn-outline-info" type="button" role="tab" data-target="#stations"
+                    aria-controls="stations" aria-selected="false" aria-pressed="false"><i class="mr-1 fas fa-sign"></i>查站牌
+            </button>
+            <button class="btn btn-outline-info dropdown-toggle dropdown-toggle-split" type="button"
+                    data-toggle="dropdown" aria-expanded="false">
               <span class="sr-only">切换下拉菜单</span>
             </button>
             <div class="p-3 dropdown-menu dropdown-menu-right">
@@ -35,7 +43,9 @@ require_once dirname(dirname(__DIR__)) . "/header.php";
             </div>
           </div>
           <div class="btn-group btn-group-sm dropdown">
-            <button class="btn btn-outline-danger" type="button" role="tab" data-target="#transfer" aria-controls="transfer" aria-selected="false" aria-pressed="false"><i class="mr-1 fas fa-bus"></i>查换乘</button>
+            <button class="btn btn-outline-danger" type="button" role="tab" data-target="#transfer"
+                    aria-controls="transfer" aria-selected="false" aria-pressed="false"><i class="mr-1 fas fa-map-signs"></i>查换乘
+            </button>
           </div>
         </div>
       </div>
@@ -43,48 +53,59 @@ require_once dirname(dirname(__DIR__)) . "/header.php";
         <div class="tab-content">
           <div class="tab-pane fade show active" id="lines">
             <div class="input-group">
-              <label class="input-group-text btn btn-lg btn-primary" for="getRelatedLines" data-toggle="collapse" data-target="#getRelatedLinesCollapse">线路</label>
-              <input type="text" class="form-control form-control-lg" placeholder="例如：3" id="getRelatedLines" pattern="/^\d{1,}$/" autocomplete="off" maxlength="15" aria-label="线路名称">
+              <label class="input-group-text btn btn-lg btn-primary" for="getRelatedLines" data-toggle="collapse"
+                     data-target="#getRelatedLinesCollapse">线路</label>
+              <input type="text" class="form-control form-control-lg" placeholder="例如：3" id="getRelatedLines"
+                     pattern="/^\d{1,}$/" autocomplete="off" maxlength="15" aria-label="线路名称">
               <span class="input-group-text">路车</span>
             </div>
             <div class="fade collapse" id="getRelatedLinesCollapse">
               <div class="my-2 text-muted small font-weight-bolder">
-                <div>要查询的公交线路，想乘坐2路车，输入数字 2</div>
+                <div>要查询的公交线路，返回实时路线</div>
+                <div>例如：想乘坐2路车，输入数字 2</div>
               </div>
             </div>
           </div>
           <div class="tab-pane fade" id="stations">
             <div class="input-group">
-              <label class="input-group-text btn btn-lg btn-info" for="getRelatedStations" data-toggle="collapse" data-target="#getRelatedStationsCollapse">站牌</label>
-              <input type="text" class="form-control form-control-lg" placeholder="例如：人民医院" id="getRelatedStations" autocomplete="off" maxlength="15" aria-label="站牌名称">
+              <label class="input-group-text btn btn-lg btn-info" for="getRelatedStations" data-toggle="collapse"
+                     data-target="#getRelatedStationsCollapse">站牌</label>
+              <input type="text" class="form-control form-control-lg" placeholder="例如：人民医院" id="getRelatedStations"
+                     autocomplete="off" maxlength="15" aria-label="站牌名称">
             </div>
             <div class="fade collapse" id="getRelatedStationsCollapse">
               <div class="my-2 text-muted small font-weight-bolder">
-                <div>请输入站牌名称，例如想查看途经人民医院的线路，输入 人民医院。</div>
-                <div>输入完成后，在页面空白处点击一次即可开始查询。</div>
+                <div>要查询的站牌名称，返回途径的公交车</div>
+                <div>例如：想查看途经人民医院的公交车，输入 人民医院。</div>
               </div>
             </div>
           </div>
           <div class="tab-pane fade" id="transfer">
             <div class="mb-3">
               <div class="input-group mb-1">
-                <label class="input-group-text btn btn-lg btn-success" for="startingStationName" data-toggle="collapse" data-target="#startingStationNameCollapse">起点</label>
-                <input type="text" class="form-control form-control-lg" placeholder="例如：人民医院" id="startingStationName" autocomplete="off" aria-label="起点站牌名称">
+                <label class="input-group-text btn btn-lg btn-success" for="startingStationName" data-toggle="collapse"
+                       data-target="#startingStationNameCollapse">起点</label>
+                <input type="text" class="form-control form-control-lg" placeholder="例如：人民医院" id="startingStationName"
+                       autocomplete="off" aria-label="起点站牌名称">
               </div>
               <div class="fade collapse" id="startingStationNameCollapse">
                 <div class="my-2 text-muted small font-weight-bolder">
-                  <div>请输入起始的站牌名称，例如想从市三院开始坐车，输入 市三院。</div>
+                  <div>上车的站牌名称</div>
+                  <div>例如：准备从市三院开始坐车，输入 市三院。</div>
                 </div>
               </div>
             </div>
             <div class="mb-3">
               <div class="input-group mb-1">
-                <label class="input-group-text btn btn-lg btn-danger" for="stopStationName" data-toggle="collapse" data-target="#stopStationNameCollapse">终点</label>
-                <input type="text" class="form-control form-control-lg" placeholder="例如：医院" id="stopStationName" autocomplete="off" aria-label="终点站牌名称">
+                <label class="input-group-text btn btn-lg btn-danger" for="stopStationName" data-toggle="collapse"
+                       data-target="#stopStationNameCollapse">终点</label>
+                <input type="text" class="form-control form-control-lg" placeholder="例如：医院" id="stopStationName"
+                       autocomplete="off" aria-label="终点站牌名称">
               </div>
               <div class="fade collapse" id="stopStationNameCollapse">
                 <div class="my-2 text-muted small font-weight-bolder">
-                  <div>请输入终止的站牌名称，例如想从人民医院下车，输入 人民医院。</div>
+                  <div>下车的站牌名称</div>
+                  <div>例如：准备从人民医院下车，输入 人民医院。</div>
                 </div>
               </div>
             </div>
