9d83e2075f72592836c00ae00b68d2cfcaf0b606
zhangchenglin <469946668@qq.com>
2020-10-04 02:23:27 +0800

    调整 添加政策首页

---
+++ wwwroot/policies/index.php
@@ -0,0 +1,14 @@
+<?php
+define('title', '服务条款和隐私权利');
+//define('descriptionContent', '');
+//define('keywordsContent', '');
+require_once dirname(__DIR__) . '/common/blank/header.php';
+if (!defined('SITE_HEAD')) die();
+?>
+<div class="d-none">
+  <?php require_once dirname(__DIR__) . "/javascript.php"; ?>
+</div>
+
+<?php
+require_once dirname(__DIR__) . '/common/blank/footer.php';
+?>
