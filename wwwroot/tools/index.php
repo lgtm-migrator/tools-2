4e51b7cca2948a940a1d675dcbd77fae2192f7ef
zhangchenglin <469946668@qq.com>
2020-10-03 14:28:42 +0800

    调整 添加TDK完善和增加工具

--- wwwroot/tools/index.php
+++ wwwroot/tools/index.php
@@ -1,7 +1,7 @@
 <?php
 define('title', '在线小工具');
-define('descriptionContent', '');
-define('keywordsContent', '');
+define('descriptionContent', '为广大职工提供实用的小工具和信息查询服务。');
+define('keywordsContent', '考试酷,应急安全知识竞赛,杰格电话本');
 require_once dirname(__DIR__) . '/header.php';
 if (!defined('SITE_HEAD')) die();
 ?>
@@ -45,12 +45,23 @@ if (!defined('SITE_HEAD')) die();
       <div class="card h-100 hvr-wobble-bottom">
         <div class="d-flex justify-content-between card-header py-2">
           <a class="stretched-link text-dark font-weight-bold text-decoration-none" href="/exam_answer_query/"
-             title="考试信息参考" style="font-size: 95%;"><i
-              class="mr-2 text-success fa-lg fas fa-check-square"></i>考试信息参考</a>
+             title="考试信息参考" style="font-size: 95%;"><i class="mr-2 text-success fa-lg fas fa-check-square"></i>考试信息参考</a>
           <div class="category_name text-muted small">[&nbsp;查询&nbsp;]</div>
         </div>
         <div class="card-body">
           <p class="card-desc card-text small">快速查询参考答案。</p>
+        </div>
+      </div>
+    </div>
+    <div class="col mb-3 tools-collapse collapse-enquiry collapse">
+      <div class="card h-100 hvr-wobble-bottom">
+        <div class="d-flex justify-content-between card-header py-2">
+          <a class="stretched-link text-dark font-weight-bold text-decoration-none" href="/yjt/"
+             title="河北省2020年应急安全知识网络竞赛参考答案" style="font-size: 95%;"><i class="mr-2 text-success fa-lg fas fa-check-square"></i>应急网络竞赛参考</a>
+          <div class="category_name text-muted small">[&nbsp;查询&nbsp;]</div>
+        </div>
+        <div class="card-body">
+          <p class="card-desc card-text small">河北省2020年应急安全知识网络竞赛参考答案。</p>
         </div>
       </div>
     </div>
