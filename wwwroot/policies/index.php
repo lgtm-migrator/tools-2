a442522d88183de9fcc00be0e2fdef4ed4c57f55
zhangchenglin <469946668@qq.com>
2020-10-09 21:41:39 +0800

    调整 添加最低高度填充

--- wwwroot/policies/index.php
+++ wwwroot/policies/index.php
@@ -1,10 +1,11 @@
 <?php
 define('title', '服务条款和隐私权利');
-//define('descriptionContent', '');
-//define('keywordsContent', '');
+define('descriptionContent', '');
+define('keywordsContent', '');
 require_once dirname(__DIR__) . '/common/blank/header.php';
 if (!defined('SITE_HEAD')) die();
 ?>
+<div class="container min-vh-90"></div>
 <div class="d-none">
   <?php require_once dirname(__DIR__) . "/javascript.php"; ?>
 </div>
