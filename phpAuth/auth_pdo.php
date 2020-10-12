077b46712e95439f941614edc244f068b91e7506
zhangchenglin <469946668@qq.com>
2020-10-13 04:25:14 +0800

    调整 修复字段使用错误

--- phpAuth/auth_pdo.php
+++ phpAuth/auth_pdo.php
@@ -9,13 +9,13 @@ require_once dirname(__DIR__) . '/config/env_phpauth.php';
 // 数据库类型
 $dbms = $_ENV['PHPAuth_DB_CONNECTION'];
 // 数据库主机名
-$host = $_ENV['PHPAuth_DB_CONNECTION'];
+$host = $_ENV['PHPAuth_DB_HOST'];
 // 使用的数据库名
-$dbName = $_ENV['PHPAuth_DB_CONNECTION'];
+$dbName = $_ENV['PHPAuth_DB_DATABASE_NAME'];
 // 数据库连接用户名
-$user = $_ENV['PHPAuth_DB_CONNECTION'];
+$user = $_ENV['PHPAuth_DB_USERNAME'];
 // 对应的密码
-$pass = $_ENV['PHPAuth_DB_CONNECTION'];
+$pass = $_ENV['PHPAuth_DB_PASSWORD'];
 
 $dsn = "$dbms:host=$host;dbname=$dbName";
 
